29ec5fab3139197664ed615f39589a7626892a25
Paddy Byers <paddy.byers@gmail.com>
2015-10-25 14:18:23 +0000

    Improve error logging

--- android-test/src/androidTest/java/io/ably/lib/test/android/AssetResourceLoader.java
+++ android-test/src/androidTest/java/io/ably/lib/test/android/AssetResourceLoader.java
@@ -2,21 +2,30 @@ package io.ably.lib.test.android;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.util.Log;
+
 import java.io.InputStream;
 import java.io.IOException;
 import io.ably.lib.test.common.ResourceLoader;
 
 public class AssetResourceLoader implements ResourceLoader {
 	public byte[] read(String resourceName) throws IOException {
-		InputStream is = instrumentationCtx.getAssets().open(resourceName);
+		InputStream is = null;
+		byte[] bytes = null;
 		try {
-			byte[] bytes = new byte[is.available()];
+			is = instrumentationCtx.getAssets().open(resourceName);
+			Log.v(TAG, "Reading " + is.available() + " bytes for resource " + resourceName);
+			bytes = new byte[is.available()];
 			is.read(bytes);
-			return bytes;
+		} catch(IOException ioe) {
+			Log.e(TAG, "Unexpected exception reading asset resource", ioe);
 		} finally {
-			is.close();
+			if(is != null)
+				is.close();
+			return bytes;
 		}
 	}
 
+	private static final String TAG = AssetResourceLoader.class.getName();
 	private Context instrumentationCtx = InstrumentationRegistry.getContext();
 }
