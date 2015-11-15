642593a318aed0f6cfe7d7caca81362072a910ab
Paddy Byers <paddy.byers@gmail.com>
2015-11-15 08:25:32 +0000

    Add async publish test

--- lib/src/test/java/io/ably/lib/test/rest/RestChannelPublishTest.java
+++ lib/src/test/java/io/ably/lib/test/rest/RestChannelPublishTest.java
@@ -2,19 +2,26 @@ package io.ably.lib.test.rest;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 import io.ably.lib.rest.AblyRest;
 import io.ably.lib.rest.Channel;
 import io.ably.lib.test.common.Setup;
+import io.ably.lib.test.common.Helpers.AsyncWaiter;
+import io.ably.lib.test.common.Helpers.CompletionSet;
 import io.ably.lib.test.common.Setup.TestVars;
 import io.ably.lib.types.AblyException;
+import io.ably.lib.types.AsyncPaginatedResult;
 import io.ably.lib.types.ClientOptions;
+import io.ably.lib.types.ErrorInfo;
 import io.ably.lib.types.Message;
 import io.ably.lib.types.PaginatedResult;
 
 import java.util.HashMap;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -41,7 +48,16 @@ public class RestChannelPublishTest {
 		opts_binary.tlsPort = testVars.tlsPort;
 		opts_binary.tls = testVars.tls;
 		ably_binary = new AblyRest(opts_binary);
-}
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		if(ably_text != null)
+			ably_text.dispose();
+
+		if(ably_binary != null)
+			ably_binary.dispose();
+	}
 
 	/**
 	 * Publish events with data of various datatypes using text protocol
@@ -49,10 +65,11 @@ public class RestChannelPublishTest {
 	@Test
 	public void channelpublish_text() {
 		/* first, publish some messages */
-		Channel publish0 = ably_text.channels.get("persisted:publish0");
+		String channelName = "persisted:channelpublish_text";
+		Channel pubChannel = ably_text.channels.get(channelName);
 		try {
-			publish0.publish("publish0", "This is a string message payload");
-			publish0.publish("publish1", "This is a byte[] message payload".getBytes());
+			pubChannel.publish("pub_text", "This is a string message payload");
+			pubChannel.publish("pub_binary", "This is a byte[] message payload".getBytes());
 		} catch(AblyException e) {
 			e.printStackTrace();
 			fail("channelpublish_text: Unexpected exception");
@@ -61,15 +78,15 @@ public class RestChannelPublishTest {
 
 		/* get the history for this channel */
 		try {
-			PaginatedResult<Message> messages = publish0.history(null);
+			PaginatedResult<Message> messages = pubChannel.history(null);
 			assertNotNull("Expected non-null messages", messages);
 			assertEquals("Expected 2 messages", messages.items().length, 2);
 			HashMap<String, Object> messageContents = new HashMap<String, Object>();
 			/* verify message contents */
 			for(Message message : messages.items())
 				messageContents.put(message.name, message.data);
-			assertEquals("Expect publish0 to be expected String", messageContents.get("publish0"), "This is a string message payload");
-			assertEquals("Expect publish1 to be expected byte[]", new String((byte[])messageContents.get("publish1")), "This is a byte[] message payload");
+			assertEquals("Expect pub_text to be expected String", messageContents.get("pub_text"), "This is a string message payload");
+			assertEquals("Expect pub_binary to be expected byte[]", new String((byte[])messageContents.get("pub_binary")), "This is a byte[] message payload");
 		} catch (AblyException e) {
 			e.printStackTrace();
 			fail("channelpublish_text: Unexpected exception");
@@ -83,10 +100,11 @@ public class RestChannelPublishTest {
 	@Test
 	public void channelpublish_binary() {
 		/* first, publish some messages */
-		Channel publish1 = ably_binary.channels.get("persisted:publish1");
+		String channelName = "persisted:channelpublish_binary";
+		Channel pubChannel = ably_binary.channels.get(channelName);
 		try {
-			publish1.publish("publish0", "This is a string message payload");
-			publish1.publish("publish1", "This is a byte[] message payload".getBytes());
+			pubChannel.publish("pub_text", "This is a string message payload");
+			pubChannel.publish("pub_binary", "This is a byte[] message payload".getBytes());
 		} catch(AblyException e) {
 			e.printStackTrace();
 			fail("channelpublish_binary: Unexpected exception");
@@ -95,19 +113,61 @@ public class RestChannelPublishTest {
 
 		/* get the history for this channel */
 		try {
-			PaginatedResult<Message> messages = publish1.history(null);
+			PaginatedResult<Message> messages = pubChannel.history(null);
 			assertNotNull("Expected non-null messages", messages);
 			assertEquals("Expected 2 messages", messages.items().length, 2);
 			HashMap<String, Object> messageContents = new HashMap<String, Object>();
 			/* verify message contents */
 			for(Message message : messages.items())
 				messageContents.put(message.name, message.data);
-			assertEquals("Expect publish0 to be expected String", messageContents.get("publish0"), "This is a string message payload");
-			assertEquals("Expect publish1 to be expected byte[]", new String((byte[])messageContents.get("publish1")), "This is a byte[] message payload");
+			assertEquals("Expect pub_text to be expected String", messageContents.get("pub_text"), "This is a string message payload");
+			assertEquals("Expect pub_binary to be expected byte[]", new String((byte[])messageContents.get("pub_binary")), "This is a byte[] message payload");
 		} catch (AblyException e) {
 			e.printStackTrace();
 			fail("channelpublish_binary: Unexpected exception");
 			return;
 		}
+	}
+
+	/**
+	 * Publish events using the async publish API
+	 */
+	@Test
+	public void channelpublish_async() {
+		/* first, publish some messages */
+		String channelName = "persisted:channelpublish_async";
+		String textPayload = "This is a string message payload";
+		byte[] binaryPayload = "This is a byte[] message payload".getBytes();
+		Channel pubChannel = ably_binary.channels.get(channelName);
+		CompletionSet pubComplete = new CompletionSet();
+
+		pubChannel.publishAsync(new Message[] {new Message("pub_text", textPayload)}, pubComplete.add());
+		pubChannel.publishAsync(new Message[] {new Message("pub_binary", binaryPayload)}, pubComplete.add());
+
+		ErrorInfo[] pubErrors = pubComplete.waitFor();
+		if(pubErrors != null && pubErrors.length > 0) {
+			fail("channelpublish_async: Unexpected errors from publish");
+			return;
+		}
+
+		/* get the history for this channel */
+		AsyncWaiter<AsyncPaginatedResult<Message>> callback = new AsyncWaiter<AsyncPaginatedResult<Message>>();
+		pubChannel.historyAsync(null, callback);
+		callback.waitFor();
+
+		if(callback.error != null) {
+			fail("channelpublish_async: Unexpected errors from history: " + callback.error);
+			return;
+		}
+
+		AsyncPaginatedResult<Message> messages = callback.result;
+		assertNotNull("Expected non-null messages", messages);
+		assertEquals("Expected 2 messages", messages.items().length, 2);
+		HashMap<String, Object> messageContents = new HashMap<String, Object>();
+		/* verify message contents */
+		for(Message message : messages.items())
+			messageContents.put(message.name, message.data);
+		assertEquals("Expect pub_text to be expected String", messageContents.get("pub_text"), textPayload);
+		assertThat("Expect pub_binary to be expected byte[]", (byte[])messageContents.get("pub_binary"), equalTo(binaryPayload));
 	}
 }
