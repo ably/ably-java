7cb9858e51965b57c7fb3aac04deae01d9720d54
Paddy Byers <paddy.byers@gmail.com>
2016-03-16 10:54:35 +0000

    Add proxy support for AsyncHttp APIs; move authorisation so it occurs for Ably APIs only; return Futures from async calls so requests are cancellable

--- lib/src/main/java/io/ably/lib/http/AsyncHttp.java
+++ lib/src/main/java/io/ably/lib/http/AsyncHttp.java
@@ -1,12 +1,19 @@
 package io.ably.lib.http;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.Proxy;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import io.ably.lib.http.Http.RequestBody;
 import io.ably.lib.http.Http.ResponseHandler;
+import io.ably.lib.transport.Hosts;
 import io.ably.lib.types.AblyException;
 import io.ably.lib.types.Callback;
 import io.ably.lib.types.ErrorInfo;
@@ -23,8 +30,8 @@ public class AsyncHttp extends ThreadPoolExecutor {
 	 * @param responseHandler
 	 * @param callback
 	 */
-	public <T> void get(String path, Param[] headers, Param[] params, ResponseHandler<T> responseHandler, Callback<T> callback) {
-		ablyHttpExecute(path, Http.GET, headers, params, null, responseHandler, callback);
+	public <T> Future<T> get(String path, Param[] headers, Param[] params, ResponseHandler<T> responseHandler, Callback<T> callback) {
+		return ablyHttpExecuteWithFallback(path, Http.GET, headers, params, null, responseHandler, callback);
 	}
 
 	/**
@@ -36,8 +43,8 @@ public class AsyncHttp extends ThreadPoolExecutor {
 	 * @param responseHandler
 	 * @param callback
 	 */
-	public <T> void post(String path, Param[] headers, Param[] params, RequestBody requestBody, ResponseHandler<T> responseHandler, Callback<T> callback) {
-		ablyHttpExecute(path, Http.POST, headers, params, requestBody, responseHandler, callback);
+	public <T> Future<T> post(String path, Param[] headers, Param[] params, RequestBody requestBody, ResponseHandler<T> responseHandler, Callback<T> callback) {
+		return ablyHttpExecuteWithFallback(path, Http.POST, headers, params, requestBody, responseHandler, callback);
 	}
 
 	/**
@@ -48,13 +55,262 @@ public class AsyncHttp extends ThreadPoolExecutor {
 	 * @param responseHandler
 	 * @param callback
 	 */
-	public <T> void del(String path, Param[] headers, Param[] params, ResponseHandler<T> responseHandler, Callback<T> callback) {
-		ablyHttpExecute(path, Http.DELETE, headers, params, null, responseHandler, callback);
+	public <T> Future<T> del(String path, Param[] headers, Param[] params, ResponseHandler<T> responseHandler, Callback<T> callback) {
+		return ablyHttpExecuteWithFallback(path, Http.DELETE, headers, params, null, responseHandler, callback);
 	}
 
 	/**************************
 	 *     Internal API
 	 **************************/
+
+	/**
+	 * An AsyncRequest type representing a request to a specific URL
+	 * @param <T>
+	 */
+	private class UrlRequest<T> extends AsyncRequest<T> implements Runnable {
+		private UrlRequest(
+				URL url,
+				final Proxy proxy,
+				final String method,
+				final Param[] headers,
+				final Param[] params,
+				final RequestBody requestBody,
+				final boolean withCredentials,
+				final ResponseHandler<T> responseHandler,
+				final Callback<T> callback) {
+			super(proxy, method, headers, params, requestBody, withCredentials, responseHandler, callback);
+			this.url = url;
+		}
+		@Override
+		public void run() {
+			try {
+				createConnection(url);
+				T result = httpExecute();
+				setResult(result);
+			} catch(AblyException e) {
+				setError(e.errorInfo);
+			} finally {
+				disposeConnection();
+			}
+		}
+		private final URL url;
+	}
+
+	/**
+	 * An AsyncRequest type representing a request to an Ably endpoint specified by host and path,
+	 * supporting reauthentication on receipt of WWW-Authenticate
+	 * @param <T>
+	 */
+	private class AblyRequestWithRetry<T> extends AsyncRequest<T> implements Runnable {
+		private AblyRequestWithRetry(
+				String host,
+				String path,
+				final Proxy proxy,
+				final String method,
+				final Param[] headers,
+				final Param[] params,
+				final RequestBody requestBody,
+				final ResponseHandler<T> responseHandler,
+				final Callback<T> callback) {
+			super(proxy, method, headers, params, requestBody, true, responseHandler, callback);
+			this.host = host;
+			this.path = path;
+		}
+		@Override
+		public void run() {
+			try {
+				result = httpExecuteWithRetry(host, path);
+				setResult(result);
+			} catch(AblyException e) {
+				setError(e.errorInfo);
+			} finally {
+				disposeConnection();
+			}
+		}
+		private final String host;
+		private final String path;
+	}
+
+	/**
+	 * An AsyncRequest type representing a request to an Ably endpoint specified by path,
+	 * supporting host fallback and reauthentication on receipt of WWW-Authenticate
+	 * @param <T>
+	 */
+	private class AblyRequestWithFallback<T> extends AsyncRequest<T> implements Runnable {
+		private AblyRequestWithFallback(
+				String path,
+				final Proxy proxy,
+				final String method,
+				final Param[] headers,
+				final Param[] params,
+				final RequestBody requestBody,
+				final ResponseHandler<T> responseHandler,
+				final Callback<T> callback) {
+			super(proxy, method, headers, params, requestBody, true, responseHandler, callback);
+			this.path = path;
+		}
+		@Override
+		public void run() {
+			int retryCountRemaining = Hosts.isRestFallbackSupported(http.host) ? http.options.httpMaxRetryCount : 0;
+			String candidateHost = http.host;
+
+			while(!isCancelled) {
+				try {
+					result = httpExecuteWithRetry(candidateHost, path);
+					setResult(result);
+					break;
+				} catch (AblyException.HostFailedException e) {
+					if(--retryCountRemaining < 0) {
+						setError(new ErrorInfo("Connection failed; no host available", 404, 80000));
+						break;
+					}
+					Log.d(TAG, "Connection failed to host `" + candidateHost + "`. Searching for new host...");
+					candidateHost = Hosts.getFallback(candidateHost);
+					Log.d(TAG, "Switched to `" + candidateHost + "`.");
+				} catch(AblyException e) {
+					setError(e.errorInfo);
+					break;
+				} finally {
+					disposeConnection();
+				}
+			}
+		}
+		private final String path;
+	}
+
+	/**
+	 * A class encapsulating a scheduled or in-process async HTTP request
+	 * @param <T>
+	 */
+	private abstract class AsyncRequest<T> implements Future<T> {
+		private AsyncRequest(
+				final Proxy proxy,
+				final String method,
+				final Param[] headers,
+				final Param[] params,
+				final RequestBody requestBody,
+				final boolean withCredentials,
+				final ResponseHandler<T> responseHandler,
+				final Callback<T> callback) {
+			this.proxy = (proxy == null) ? Proxy.NO_PROXY : proxy;
+			this.method = method;
+			this.headers = headers;
+			this.params = params;
+			this.requestBody = requestBody;
+			this.withCredentials = withCredentials;
+			this.responseHandler = responseHandler;
+			this.callback = callback;
+		}
+
+		/**************************
+		 *    Future<T> methods
+		 **************************/
+		@Override
+		public boolean cancel(boolean mayInterruptIfRunning) {
+			isCancelled = true;
+			return disposeConnection();
+		}
+		@Override
+		public boolean isCancelled() {
+			return isCancelled;
+		}
+		@Override
+		public boolean isDone() {
+			return isDone;
+		}
+		@Override
+		public T get() throws InterruptedException, ExecutionException {
+			synchronized(this) {
+				while(!isDone) {
+					wait();
+				}
+				if(err != null) {
+					throw new ExecutionException(AblyException.fromErrorInfo(err));
+				}
+			}
+			return result;
+		}
+		@Override
+		public T get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+			long remaining = unit.toMillis(timeout), deadline = System.currentTimeMillis() + remaining;
+			synchronized(this) {
+				while(remaining > 0) {
+					wait(remaining);
+					if(isDone) { break; }
+					remaining = deadline - System.currentTimeMillis();
+				}
+				if(!isDone) {
+					throw new TimeoutException();
+				}
+				if(err != null) {
+					throw new ExecutionException(AblyException.fromErrorInfo(err));
+				}
+			}
+			return result;
+		}
+
+		/**************************
+		 *        Private
+		 **************************/
+
+		protected synchronized void createConnection(URL url) throws AblyException {
+			try {
+				conn = (HttpURLConnection)url.openConnection(proxy);
+			} catch(IOException ioe) {
+				throw AblyException.fromThrowable(ioe);
+			}
+		}
+		protected T httpExecute() throws AblyException {
+			return http.httpExecute(conn, method, headers, requestBody, withCredentials, responseHandler);
+		}
+		protected T httpExecuteWithRetry(String host, String path) throws AblyException {
+			URL url = Http.buildURL(http.scheme, host, http.port, path, params);
+			return http.httpExecuteWithRetry(url, proxy, method, headers, requestBody, responseHandler);
+		}
+		protected void setResult(T result) {
+			synchronized(this) {
+				this.result = result;
+				this.isDone = true;
+				notifyAll();
+			}
+			if(callback != null) {
+				callback.onSuccess(result);
+			}
+		}
+		protected void setError(ErrorInfo err) {
+			synchronized(this) {
+				this.err = err;
+				this.isDone = true;
+				notifyAll();
+			}
+			if(callback != null) {
+				callback.onError(err);
+			}
+		}
+		protected synchronized boolean disposeConnection() {
+			boolean hasConnection = conn != null;
+			if(hasConnection) {
+				conn.disconnect();
+				conn = null;
+			}
+			return hasConnection;
+		}
+
+		protected HttpURLConnection conn;
+		protected T result;
+		protected ErrorInfo err;
+
+		protected final Proxy proxy;
+		protected final String method;
+		protected final Param[] headers;
+		protected final Param[] params;
+		protected final RequestBody requestBody;
+		protected final boolean withCredentials;
+		protected final ResponseHandler<T> responseHandler;
+		protected final Callback<T> callback;
+		protected boolean isCancelled = false;
+		protected boolean isDone = false;
+}
 
 	public AsyncHttp(Http http) {
 		super(DEFAULT_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
@@ -65,7 +321,18 @@ public class AsyncHttp extends ThreadPoolExecutor {
 		this.setCorePoolSize(size);
 	}
 
-	public <T> void httpExecute(
+	/**
+	 * Make an asynchronous HTTP request to a given URL
+	 * @param url
+	 * @param method
+	 * @param headers
+	 * @param requestBody
+	 * @param withCredentials
+	 * @param responseHandler
+	 * @param callback
+	 * @return
+	 */
+	public <T> Future<T> httpExecute(
 			final URL url,
 			final String method,
 			final Param[] headers,
@@ -74,22 +341,23 @@ public class AsyncHttp extends ThreadPoolExecutor {
 			final ResponseHandler<T> responseHandler,
 			final Callback<T> callback) {
 
-		submit(new Runnable() {
-			@Override
-			public void run() {
-				T result = null;
-				try {
-					result = http.httpExecute(url, method, headers, requestBody, withCredentials, responseHandler);
-				} catch(AblyException e) {
-					callback.onError(e.errorInfo);
-					return;
-				}
-				callback.onSuccess(result);
-			}
-		});
+		UrlRequest<T> request = new UrlRequest<>(url, http.proxy, method, headers, null, requestBody, withCredentials, responseHandler, callback);
+		execute(request);
+		return request;
 	}
 
-	public <T> void ablyHttpExecute(
+	/**
+	 * Make an asynchronous HTTP request to an Ably endpoint, using the Ably auth credentials and fallback hosts if necessary
+	 * @param path
+	 * @param method
+	 * @param headers
+	 * @param params
+	 * @param requestBody
+	 * @param responseHandler
+	 * @param callback
+	 * @return
+	 */
+	public <T> Future<T> ablyHttpExecuteWithFallback(
 			final String path,
 			final String method,
 			final Param[] headers,
@@ -98,26 +366,36 @@ public class AsyncHttp extends ThreadPoolExecutor {
 			final ResponseHandler<T> responseHandler,
 			final Callback<T> callback) {
 
-		submit(new Runnable() {
-			@Override
-			public void run() {
-				T result = null;
-				ErrorInfo error = null;
-				try {
-					result = http.ablyHttpExecute(path, method, headers, params, requestBody, responseHandler);
-				} catch(AblyException e) {
-					error = e.errorInfo;
-				}
-				try {
-					if(error != null)
-						callback.onError(error);
-					else
-						callback.onSuccess(result);
-				} catch(Throwable t) {
-					Log.e(TAG, "Exception invoking callback", t);
-				}
-			}
-		});
+		AblyRequestWithFallback<T> request = new AblyRequestWithFallback<>(path, http.proxy, method, headers, null, requestBody, responseHandler, callback);
+		execute(request);
+		return request;
+	}
+
+	/**
+	 * Make an asynchronous HTTP request to an Ably endpoint, using the Ably auth credentials and reauthentication if necessary
+	 * @param host
+	 * @param path
+	 * @param method
+	 * @param headers
+	 * @param params
+	 * @param requestBody
+	 * @param responseHandler
+	 * @param callback
+	 * @return
+	 */
+	public <T> Future<T> ablyHttpExecuteWithRetry(
+			final String host,
+			final String path,
+			final String method,
+			final Param[] headers,
+			final Param[] params,
+			final RequestBody requestBody,
+			final ResponseHandler<T> responseHandler,
+			final Callback<T> callback) {
+
+		AblyRequestWithRetry<T> request = new AblyRequestWithRetry<>(host, path, http.proxy, method, headers, null, requestBody, responseHandler, callback);
+		execute(request);
+		return request;
 	}
 
 	public void dispose() {
