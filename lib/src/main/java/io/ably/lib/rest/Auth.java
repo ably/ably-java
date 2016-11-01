b995023e6ffee79b1874a780c8e9ecb3adceeb7d
Tim Renouf <tpr.ably@botech.co.uk>
2016-11-01 10:46:16 +0000

    Reword deprecation warning for authorise() alias

--- lib/src/main/java/io/ably/lib/rest/Auth.java
+++ lib/src/main/java/io/ably/lib/rest/Auth.java
@@ -429,7 +429,7 @@ public class Auth {
 	 * Alias of authorize() (0.9 RSA10l)
 	 */
 	public TokenDetails authorise(TokenParams params, AuthOptions options) throws AblyException {
-		Log.w(TAG, "authorise() alias will be removed in 1.0");
+		Log.w(TAG, "authorise() is deprecated and will be removed in 1.0. Please use authorize() instead");
 		return authorize(params, options);
 	}
 
