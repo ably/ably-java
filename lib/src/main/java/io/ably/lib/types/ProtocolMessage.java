2e2791cb1b1762a4b08b5a031512663c178240d3
Tim Renouf <tpr.ably@botech.co.uk>
2016-11-03 10:02:30 +0000

    0.9 resumed flag
    RTL2f: ChannelStateChange.resumed is set from the resumed flag in the
    attached protocol message.
    
    No tests for this yet. At the time of committing, the server does not
    set the flag in the message yet.

--- lib/src/main/java/io/ably/lib/types/ProtocolMessage.java
+++ lib/src/main/java/io/ably/lib/types/ProtocolMessage.java
@@ -50,7 +50,8 @@ public class ProtocolMessage {
 
 	public enum Flag {
 		has_presence,
-		has_backlog;
+		has_backlog,
+		resumed;
 
 		public int getValue() { return ordinal(); }
 		public static Flag findByValue(int value) { return values()[value]; }
