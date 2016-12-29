e2e2562425983416911231b17df24a2b5598249e
Paddy Byers <paddy.byers@gmail.com>
2016-12-29 23:57:32 +0000

    Include issue references in comment

--- android/src/main/java/io/ably/lib/util/Platform.java
+++ android/src/main/java/io/ably/lib/util/Platform.java
@@ -3,7 +3,11 @@ package io.ably.lib.util;
 import android.os.Build;
 
 public class Platform {
-	/* The msgpack-core library at 0.8.11 has issues on 4.4.2 and probably earlier */
+	/*
+	 * The msgpack-core library at 0.8.11 has issues on 4.4.2 and probably earlier;
+	 * see: https://github.com/ably/ably-java/issues/261
+	 *      https://github.com/msgpack/msgpack-java/issues/406
+	 */
 	public static boolean supportsMsgpack() {
 		return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP);
 	}
