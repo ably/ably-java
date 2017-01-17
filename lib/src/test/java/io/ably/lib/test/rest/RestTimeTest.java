e918b09f45d30b0dbbcfb521a84bd688461b9081
Paddy Byers <paddy.byers@gmail.com>
2017-01-17 16:10:13 +0000

    Ensure tests do not instance Ably with no means of authentication

--- lib/src/test/java/io/ably/lib/test/rest/RestTimeTest.java
+++ lib/src/test/java/io/ably/lib/test/rest/RestTimeTest.java
@@ -37,7 +37,7 @@ public class RestTimeTest extends ParameterizedTest {
 	@Test
 	public void time1() {
 		try {
-			ClientOptions opts = createOptions();
+			ClientOptions opts = createOptions("not:a.key");
 			AblyRest ablyNoAuth = new AblyRest(opts);
 			ablyNoAuth.time();
 		} catch (AblyException e) {
@@ -52,7 +52,7 @@ public class RestTimeTest extends ParameterizedTest {
 	@Test
 	public void time2() {
 		try {
-			ClientOptions opts = createOptions();
+			ClientOptions opts = createOptions("not:a.key");
 			opts.environment = null;
 			opts.restHost = "this.restHost.does.not.exist";
 			AblyRest ably = new AblyRest(opts);
