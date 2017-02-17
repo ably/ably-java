704415891839000b1f6ae29e33ff987cc0edbc48
Toni Cárdenas <toni@tcardenas.me>
2017-02-17 14:01:15 +0100

    Push: implement activation state machine.

---
+++ android/src/main/java/io/ably/lib/util/IntentUtil.java
@@ -0,0 +1,26 @@
+package io.ably.lib.util;
+
+import android.content.Intent;
+import io.ably.lib.types.ErrorInfo;
+
+public class IntentUtil {
+    public static void addErrorInfo(Intent intent, ErrorInfo error) {
+        intent.putExtra("hasError", error != null);
+        if (error != null) {
+            intent.putExtra("error.message", error.message);
+            intent.putExtra("error.statusCode", error.statusCode);
+            intent.putExtra("error.code", error.code);
+        }
+    }
+
+    public static ErrorInfo getErrorInfo(Intent intent) {
+        if (!intent.getBooleanExtra("hasError", false)) {
+            return null;
+        }
+        return new ErrorInfo(
+            intent.getStringExtra("error.message"),
+            intent.getIntExtra("error.statusCode", 0),
+            intent.getIntExtra("error.code", 0)
+        );
+    }
+}
