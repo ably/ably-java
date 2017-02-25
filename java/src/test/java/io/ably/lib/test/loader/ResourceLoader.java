8a6b457c574af9b0d98ac21eeadc32e39d985fc3
Paddy Byers <paddy.byers@gmail.com>
2017-02-25 18:08:32 +0000

    Allow tests to be run from top-level or project dir

--- java/src/test/java/io/ably/lib/test/loader/ResourceLoader.java
+++ java/src/test/java/io/ably/lib/test/loader/ResourceLoader.java
@@ -2,6 +2,7 @@ package io.ably.lib.test.loader;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
@@ -10,8 +11,13 @@ import java.io.IOException;
 public class ResourceLoader {
 	public byte[] read(String resourceName) throws IOException {
 		FileInputStream fis = null;
+		System.out.println("Current dir: " + new File(".").getAbsolutePath());
 		try {
-			fis = new FileInputStream(new File("../lib/src/test/resources", resourceName));
+			try {
+				fis = new FileInputStream(new File("../lib/src/test/resources", resourceName));
+			} catch(FileNotFoundException fnfe) {
+				fis = new FileInputStream(new File("lib/src/test/resources", resourceName));
+			}
 			byte[] bytes = new byte[fis.available()];
 			fis.read(bytes);
 			return bytes;
