12493757f8fdc6c3aa6be8041e032bceec7c5c9a
Matthew O'Riordan <matthew.oriordan@gmail.com>
2017-07-26 23:14:12 +0100

    ci: Use path based on Bash script

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -3,7 +3,9 @@
 set -ex
 export TERM=dumb
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 ret=0
-../gradlew java:testRestSuite || ret=1
-../gradlew java:testRealtimeSuite || ret=1
+$DIR/../gradlew java:testRestSuite || ret=1
+$DIR/../gradlew java:testRealtimeSuite || ret=1
 exit $ret
