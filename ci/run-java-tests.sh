fe13cc67a0003b9b0cf022b37ee0b11dc3df82fe
Cesare Rocchi <cesarerocchi@gmail.com>
2018-03-16 09:30:33 +0100

    Unset ANDROID_HOME

--- ci/run-java-tests.sh
+++ ci/run-java-tests.sh
@@ -1,4 +1,5 @@
-rm -rf ./android/*
+# We unset this, otherwise gradlew picks up settings also from the android "context", like here: https://travis-ci.org/ably/ably-java/jobs/353969106#L1980
+unset ANDROID_HOME
 
 ./gradlew java:testRealtimeSuite
 ./gradlew java:testRestSuite
