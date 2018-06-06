961f95fbc468725cb16ddbf3497c4456e3a4b1de
Cesare Rocchi <cesarerocchi@gmail.com>
2018-06-06 19:24:24 +0200

    Add first batch of realtime JWT tests

---
+++ lib/src/test/java/io/ably/lib/test/realtime/RealtimeJWTTest.java
@@ -0,0 +1,210 @@
+package io.ably.lib.test.realtime;
+
+import static org.junit.Assert.*;
+
+import io.ably.lib.test.common.Setup.Key;
+import io.ably.lib.types.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.ably.lib.realtime.*;
+import io.ably.lib.rest.AblyRest;
+import io.ably.lib.rest.Auth.*;
+import io.ably.lib.test.common.Helpers.*;
+import io.ably.lib.test.common.ParameterizedTest;
+
+import java.util.UUID;
+
+
+public class RealtimeJWTTest extends ParameterizedTest {
+
+	private AblyRest restJWTRequester;
+	private ClientOptions jwtRequesterOptions;
+	private ClientOptions realtimeOptions;
+	private Key key = testVars.keys[0];
+	private final String clientId = "testJWTClientID";
+	private final String channelName = "testJWTChannel" + UUID.randomUUID().toString();
+	private final String messageName = "testJWTMessage" + UUID.randomUUID().toString();
+	Param[] keys = new Param[]{ new Param("keyName", key.keyName), new Param("keySecret", key.keySecret) };
+	Param[] clientIdParam = new Param[] { new Param("clientId", clientId) };
+	Param[] shortTokenTtl = new Param[] { new Param("expiresIn", 5) };
+	private final String susbcribeOnlyCapability = "{\"" + channelName + "\": [\"subscribe\"]}";
+	private final String publishCapability = "{\"" + channelName + "\": [\"publish\"]}";
+
+	@Before
+	public void setUpBefore() throws Exception {
+		jwtRequesterOptions = createOptions(testVars.keys[0].keyStr);
+		jwtRequesterOptions.authUrl = echoServer;
+		realtimeOptions = new ClientOptions();
+		realtimeOptions.environment = "sandbox";
+	}
+
+	/**
+	 * Request a JWT that specifies a clientId
+	 * Verifies that the clientId matches the one requested
+	 */
+	@Test
+	public void auth_clientid_match_the_one_requested_in_jwt() {
+		try {
+			/* create ably realtime with JWT token */
+			realtimeOptions.token = getToken(mergeParams(keys, clientIdParam), null);
+			assertNotNull("Expected token value", realtimeOptions.token);
+			AblyRealtime ablyRealtime = new AblyRealtime(realtimeOptions);
+
+			/* wait for connected state */
+			ConnectionWaiter connectionWaiter = new ConnectionWaiter(ablyRealtime.connection);
+			connectionWaiter.waitFor(ConnectionState.connected);
+			assertEquals("Connected state was NOT reached", ConnectionState.connected, ablyRealtime.connection.state);
+
+			/* check expected clientId */
+			assertEquals("clientId does NOT match the one requested", clientId, ablyRealtime.auth.clientId);
+
+			ablyRealtime.close();
+		} catch (AblyException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	/**
+	 * Request a JWT with subscribe-only capabilities
+	 * Verifies that publishing on a channel fails
+	 */
+	@Test
+	public void auth_jwt_with_subscribe_only_capability() {
+		try {
+			/* create ably realtime with JWT token that has subscribe-only capabilities */
+			realtimeOptions.token = getToken(keys, susbcribeOnlyCapability);
+			assertNotNull("Expected token value", realtimeOptions.token);
+			final AblyRealtime ablyRealtime = new AblyRealtime(realtimeOptions);
+
+			/* wait for connected state */
+			ConnectionWaiter connectionWaiter = new ConnectionWaiter(ablyRealtime.connection);
+			connectionWaiter.waitFor(ConnectionState.connected);
+			assertEquals("Connected state was NOT reached", ConnectionState.connected, ablyRealtime.connection.state);
+
+			/* attach to channel and verify attached state */
+			Channel channel = ablyRealtime.channels.get(channelName);
+			channel.attach();
+			new ChannelWaiter(channel).waitFor(ChannelState.attached);
+
+			/* publish and verify that it fails */
+			channel.publish(messageName, null, new CompletionListener() {
+				@Override
+				public void onSuccess() {
+					fail("It should not succeed");
+				}
+
+				@Override
+				public void onError(ErrorInfo error) {
+					assertEquals("Unexpected status code", 401, error.statusCode);
+					assertEquals("Unexpected error code", 40160, error.code);
+					assertEquals("Unexpected error message", "Unable to perform channel operation (permission denied)", error.message);
+					ablyRealtime.close();
+				}
+			});
+			connectionWaiter.waitFor(ConnectionState.closed);
+		} catch (AblyException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	/**
+	 * Request a JWT with publish capabilities
+	 * Verifies that publishing on a channel succeeds
+	 */
+	@Test
+	public void auth_jwt_with_publish_capability() {
+		try {
+			/* create ably realtime with JWT token that has publish capabilities */
+			realtimeOptions.token = getToken(keys, publishCapability);
+			assertNotNull("Expected token value", realtimeOptions.token);
+			final AblyRealtime ablyRealtime = new AblyRealtime(realtimeOptions);
+
+			/* wait for connected state */
+			ConnectionWaiter connectionWaiter = new ConnectionWaiter(ablyRealtime.connection);
+			connectionWaiter.waitFor(ConnectionState.connected);
+			assertEquals("Connected state was NOT reached", ConnectionState.connected, ablyRealtime.connection.state);
+
+			/* attach to channel and verify attached state */
+			Channel channel = ablyRealtime.channels.get(channelName);
+			channel.attach();
+			new ChannelWaiter(channel).waitFor(ChannelState.attached);
+
+			/* publish, verify that it succeeds then close */
+			final Message message = new Message(messageName, null);
+			channel.publish(message, new CompletionListener() {
+				@Override
+				public void onSuccess() {
+					System.out.println("Message " + messageName + " published successfully");
+					ablyRealtime.close();
+				}
+
+				@Override
+				public void onError(ErrorInfo reason) {
+					fail("Publish should not fail");
+				}
+			});
+			connectionWaiter.waitFor(ConnectionState.closed);
+		} catch (AblyException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	/**
+	 * Request a JWT with a ttl of 5 seconds and
+	 * verify the correct error and message in the disconnected state change.
+	 */
+	@Test
+	public void auth_jwt_with_token_that_expires() {
+		try {
+			/* create ably realtime with JWT token that expires in 5 seconds */
+			realtimeOptions.token = getToken(mergeParams(keys, shortTokenTtl), null);
+			assertNotNull("Expected token value", realtimeOptions.token);
+			final AblyRealtime ablyRealtime = new AblyRealtime(realtimeOptions);
+
+			/* wait for connected state */
+			ConnectionWaiter connectionWaiter = new ConnectionWaiter(ablyRealtime.connection);
+			connectionWaiter.waitFor(ConnectionState.connected);
+			assertEquals("Connected state was NOT reached", ConnectionState.connected, ablyRealtime.connection.state);
+
+			/* Verify the expected error reason when disconnected */
+			ablyRealtime.connection.once(ConnectionEvent.disconnected, new ConnectionStateListener() {
+
+				@Override
+				public void onConnectionStateChanged(ConnectionStateChange stateChange) {
+					assertEquals("Unexpected connection stage change", 40142, stateChange.reason.code);
+					assertEquals("Unexpected error message", "Key/token status changed (expire)", stateChange.reason.message);
+					ablyRealtime.close();
+				}
+			});
+			connectionWaiter.waitFor(ConnectionState.disconnected);
+			connectionWaiter.waitFor(ConnectionState.closed);
+		} catch (AblyException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	/**
+	 * Helper to fetch a token with params via authUrl
+	 */
+	private String getToken(Param[] params, String capability) {
+		jwtRequesterOptions.authParams = params;
+		try {
+			restJWTRequester = new AblyRest(jwtRequesterOptions);
+			TokenParams t = new TokenParams();
+			if (capability != null) {
+				t.capability = capability;
+			}
+			TokenDetails tokenDetails = restJWTRequester.auth.requestToken(t, null);
+			return tokenDetails.token;
+		} catch (AblyException e) {
+			fail("Failure in fetching a JWT token" + e);
+			return null;
+		}
+	}
+
+}
