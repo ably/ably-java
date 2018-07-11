f2f3c3e7d523952a129c5a658152f45377036e85
Cesare Rocchi <cesarerocchi@gmail.com>
2018-07-11 18:32:41 +0200

    Exit with a non zero code if any of the two suites fails

--- ci/run-java-tests.sh
+++ ci/run-java-tests.sh
@@ -1,5 +1,7 @@
 # We unset this, otherwise gradlew picks up settings also from the android "context", like here: https://travis-ci.org/ably/ably-java/jobs/353969106#L1980
 unset ANDROID_HOME
 
-./gradlew java:testRealtimeSuite
-./gradlew java:testRestSuite
+ret=0
+./gradlew java:testRealtimeSuite || ret=1
+./gradlew java:testRestSuite || ret=1
+exit $ret
