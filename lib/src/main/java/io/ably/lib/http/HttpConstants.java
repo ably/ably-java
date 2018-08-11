b6422011e8e6d10901a44d696a7641eb01f41b8f
Paddy Byers <paddy.byers@gmail.com>
2018-08-11 21:51:50 +0100

    Add PATCH method

--- lib/src/main/java/io/ably/lib/http/HttpConstants.java
+++ lib/src/main/java/io/ably/lib/http/HttpConstants.java
@@ -22,6 +22,7 @@ public class HttpConstants {
 
     public static class Methods {
         public static final String GET    = "GET";
+        public static final String PATCH  = "PATCH";
         public static final String PUT    = "PUT";
         public static final String POST   = "POST";
         public static final String DELETE = "DELETE";
