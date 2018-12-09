05c4e1625c5ba28efdeed475031e2efdf602e135
Amit S <amit@smargav.com>
2018-12-09 12:31:32 +0530

    Added comments for RTE6a support

--- lib/src/main/java/io/ably/lib/util/EventEmitter.java
+++ lib/src/main/java/io/ably/lib/util/EventEmitter.java
@@ -8,107 +8,126 @@ import java.util.Map;
 
 /**
  * An interface exposing the ability to register listeners for a class of events
- * @author paddy
  *
- * @param <Event> an Enum containing the event names that listeners may be registered for
+ * @param <Event>    an Enum containing the event names that listeners may be registered for
  * @param <Listener> the interface type of the listener
+ * @author paddy
  */
 public abstract class EventEmitter<Event, Listener> {
 
-	/**
-	 * Remove all registered listeners irrespective of type
-	 */
-	public synchronized void off() {
-		listeners.clear();
-		filters.clear();
-	}
+    /**
+     * Remove all registered listeners irrespective of type
+     */
+    public synchronized void off() {
+        listeners.clear();
+        filters.clear();
+    }
 
-	/**
-	 * Register the given listener for all events
-	 * @param listener
-	 */
-	public synchronized void on(Listener listener) {
-		if(!listeners.contains(listener))
-			listeners.add(listener);
-	}
+    /**
+     * Register the given listener for all events
+     *
+     * @param listener
+     */
+    public synchronized void on(Listener listener) {
+        if (!listeners.contains(listener))
+            listeners.add(listener);
+    }
 
-	/**
-	 * Register the given listener for a single occurrence of any event
-	 * @param listener
-	 */
-	public synchronized void once(Listener listener) {
-		filters.put(listener, new Filter(null, listener, true));
-	}
+    /**
+     * Register the given listener for a single occurrence of any event
+     *
+     * @param listener
+     */
+    public synchronized void once(Listener listener) {
+        filters.put(listener, new Filter(null, listener, true));
+    }
 
-	/**
-	 * Remove a previously registered listener irrespective of type
-	 * @param listener
-	 */
-	public synchronized void off(Listener listener) {
-		listeners.remove(listener);
-		filters.remove(listener);
-	}
+    /**
+     * Remove a previously registered listener irrespective of type
+     *
+     * @param listener
+     */
+    public synchronized void off(Listener listener) {
+        listeners.remove(listener);
+        filters.remove(listener);
+    }
 
-	/**
-	 * Register the given listener for a specific event
-	 * @param listener
-	 */
-	public synchronized void on(Event event, Listener listener) {
-		filters.put(listener, new Filter(event, listener, false));
-	}
+    /**
+     * Register the given listener for a specific event
+     *
+     * @param listener
+     */
+    public synchronized void on(Event event, Listener listener) {
+        filters.put(listener, new Filter(event, listener, false));
+    }
 
-	/**
-	 * Register the given listener for a single occurrence of a specific event
-	 * @param listener
-	 */
-	public synchronized void once(Event event, Listener listener) {
-		filters.put(listener, new Filter(event, listener, true));
-	}
+    /**
+     * Register the given listener for a single occurrence of a specific event
+     *
+     * @param listener
+     */
+    public synchronized void once(Event event, Listener listener) {
+        filters.put(listener, new Filter(event, listener, true));
+    }
 
-	/**
-	 * Remove a previously registered event-specific listener
-	 * @param listener
-	 * @param event
-	 */
-	public synchronized void off(Event event, Listener listener) {
-		Filter filter = filters.get(listener);
-		if(filter != null && filter.event == event)
-			filters.remove(listener);
-	}
+    /**
+     * Remove a previously registered event-specific listener
+     *
+     * @param listener
+     * @param event
+     */
+    public synchronized void off(Event event, Listener listener) {
+        Filter filter = filters.get(listener);
+        if (filter != null && filter.event == event)
+            filters.remove(listener);
+    }
 
-	/**
-	 * Emit the given event (broadcasting to registered listeners)
-	 * @param event the Event
-	 * @param args the arguments to pass to listeners
-	 */
-	public synchronized void emit(Event event, Object... args) {
-		List<Listener> clonedListeners = new ArrayList<>();
-		clonedListeners.addAll(listeners);
-		for (int i = clonedListeners.size() - 1; i >= 0; i--) {
-			apply(clonedListeners.get(i), event, args);
-		}
+    /**
+     * Emit the given event (broadcasting to registered listeners)
+     *
+     * @param event the Event
+     * @param args  the arguments to pass to listeners
+     */
+    public synchronized void emit(Event event, Object... args) {
 
-		for(Iterator<Map.Entry<Listener, Filter>> it = filters.entrySet().iterator(); it.hasNext(); )
-			if(it.next().getValue().apply(event, args))
-				it.remove();
-	}
+        /*
+         * The set of listeners called by emit must not change over the course of the emit
+         * Refer RTE6a part of Spec for more details.
+         * To address this issue, we clone the listeners before calling emit.
+         */
+        List<Listener> clonedListeners = new ArrayList<>(listeners);
 
-	protected abstract void apply(Listener listener, Event event, Object... args);
+        for (Listener listener : clonedListeners) {
+            apply(listener, event, args);
+        }
 
-	protected class Filter {
-		Filter(Event event, Listener listener, boolean once) { this.event = event; this.listener = listener; this.once = once; }
-		private Event event;
-		private Listener listener;
-		private boolean once;
-		protected boolean apply(Event event, Object... args) {
-			if(this.event == event || this.event == null) {
-				EventEmitter.this.apply(listener, event, args);
-				return once;
-			}
-			return false;
-		}
-	}
+        for (Iterator<Map.Entry<Listener, Filter>> it = filters.entrySet().iterator(); it.hasNext(); )
+            if (it.next().getValue().apply(event, args))
+                it.remove();
+    }
 
-	Map<Listener, Filter> filters = new HashMap<Listener, Filter>();
-	List<Listener> listeners = new ArrayList<Listener>();
+    protected abstract void apply(Listener listener, Event event, Object... args);
+
+    protected class Filter {
+        Filter(Event event, Listener listener, boolean once) {
+            this.event = event;
+            this.listener = listener;
+            this.once = once;
+        }
+
+        private Event event;
+        private Listener listener;
+        private boolean once;
+
+        protected boolean apply(Event event, Object... args) {
+            if (this.event == event || this.event == null) {
+                EventEmitter.this.apply(listener, event, args);
+                return once;
+            }
+            return false;
+        }
+    }
+
+    Map<Listener, Filter> filters = new HashMap<Listener, Filter>();
+    List<Listener> listeners = new ArrayList<Listener>();
 }
--- lib/src/test/java/io/ably/lib/test/realtime/EventEmitterTest.java
+++ lib/src/test/java/io/ably/lib/test/realtime/EventEmitterTest.java
@@ -12,169 +12,187 @@ import static org.junit.Assert.assertTrue;
 
 public class EventEmitterTest {
 
-	private static enum MyEvents {
-		event_0,
-		event_1
-	}
+    private static enum MyEvents {
+        event_0,
+        event_1
+    }
 
-	private static class MyEventPayload {
-		public MyEvents event;
-		public String message;
-	}
+    private static class MyEventPayload {
+        public MyEvents event;
+        public String message;
+    }
 
-	private static interface MyListener {
-		public void onMyThingHappened(MyEventPayload theThing);
-	}
+    private static interface MyListener {
+        public void onMyThingHappened(MyEventPayload theThing);
+    }
 
-	private static class CountingListener implements MyListener {
-		@Override
-		public void onMyThingHappened(MyEventPayload theThing) {
-			Integer count = counts.get(theThing.event); if(count == null) count = 0;
-			counts.put(theThing.event, count + 1);
-		}
-		HashMap<MyEvents, Integer> counts = new HashMap<MyEvents, Integer>();
-	}
+    private static class CountingListener implements MyListener {
+        @Override
+        public void onMyThingHappened(MyEventPayload theThing) {
+            Integer count = counts.get(theThing.event);
+            if (count == null) count = 0;
+            counts.put(theThing.event, count + 1);
+        }
 
-	private static class MyEmitter extends EventEmitter<MyEvents, MyListener> {
-		@Override
-		protected void apply(MyListener listener, final MyEvents ev, final Object... args) {
-			listener.onMyThingHappened(new MyEventPayload() {{ event = ev; message = (String)args[0]; }});
-		}
-	}
+        HashMap<MyEvents, Integer> counts = new HashMap<MyEvents, Integer>();
+    }
 
-	/**
-	 * Register a listener, and verify it is called
-	 * when the event is emitted
-	 */
-	@Test
-	public void on_simple() {
-		MyEmitter emitter = new MyEmitter();
-		emitter.on(new MyListener() {
-			@Override
-			public void onMyThingHappened(MyEventPayload theThing) {
-				assertEquals(theThing.event, MyEvents.event_0);
-				assertEquals(theThing.message, "on_simple");
-			}
-		});
-		emitter.emit(MyEvents.event_0, "on_simple");
-	}
+    private static class MyEmitter extends EventEmitter<MyEvents, MyListener> {
+        @Override
+        protected void apply(MyListener listener, final MyEvents ev, final Object... args) {
+            listener.onMyThingHappened(new MyEventPayload() {{
+                event = ev;
+                message = (String) args[0];
+            }});
+        }
+    }
 
-	/**
-	 * Register a listener, and verify it is called
-	 * when the event is emitted more than once
-	 */
-	@Test
-	public void on_multiple() {
-		MyEmitter emitter = new MyEmitter();
-		CountingListener listener = new CountingListener();
-		emitter.on(listener);
-		emitter.emit(MyEvents.event_0, "on_multiple_0");
-		emitter.emit(MyEvents.event_0, "on_multiple_0");
-		emitter.emit(MyEvents.event_1, "on_multiple_1");
-		assertEquals(listener.counts.get(MyEvents.event_0), Integer.valueOf(2));
-		assertEquals(listener.counts.get(MyEvents.event_1), Integer.valueOf(1));
-	}
+    /**
+     * Register a listener, and verify it is called
+     * when the event is emitted
+     */
+    @Test
+    public void on_simple() {
+        MyEmitter emitter = new MyEmitter();
+        emitter.on(new MyListener() {
+            @Override
+            public void onMyThingHappened(MyEventPayload theThing) {
+                assertEquals(theThing.event, MyEvents.event_0);
+                assertEquals(theThing.message, "on_simple");
+            }
+        });
+        emitter.emit(MyEvents.event_0, "on_simple");
+    }
 
-	/**
-	 * Register and unregister listener, and verify it
-	 * is not called when the event is emitted
-	 */
-	@Test
-	public void off_simple() {
-		MyEmitter emitter = new MyEmitter();
-		CountingListener listener = new CountingListener();
-		emitter.on(listener);
-		emitter.off(listener);
-		emitter.emit(MyEvents.event_0, "on_multiple_0");
-		emitter.emit(MyEvents.event_1, "on_multiple_1");
-		assertNull(listener.counts.get(MyEvents.event_0));
-		assertNull(listener.counts.get(MyEvents.event_1));
-	}
+    /**
+     * Register a listener, and verify it is called
+     * when the event is emitted more than once
+     */
+    @Test
+    public void on_multiple() {
+        MyEmitter emitter = new MyEmitter();
+        CountingListener listener = new CountingListener();
+        emitter.on(listener);
+        emitter.emit(MyEvents.event_0, "on_multiple_0");
+        emitter.emit(MyEvents.event_0, "on_multiple_0");
+        emitter.emit(MyEvents.event_1, "on_multiple_1");
+        assertEquals(listener.counts.get(MyEvents.event_0), Integer.valueOf(2));
+        assertEquals(listener.counts.get(MyEvents.event_1), Integer.valueOf(1));
+    }
 
-	/**
-	 * Register and unregister multiple listeners, and verify they
-	 * are not called when the event is emitted
-	 */
-	@Test
-	public void off_all() {
-		MyEmitter emitter = new MyEmitter();
-		CountingListener listener1 = new CountingListener();
-		CountingListener listener2 = new CountingListener();
-		emitter.on(listener1);
-		emitter.on(listener2);
-		emitter.off();
-		emitter.emit(MyEvents.event_0, "on_multiple_0");
-		emitter.emit(MyEvents.event_1, "on_multiple_1");
-		assertNull(listener1.counts.get(MyEvents.event_0));
-		assertNull(listener1.counts.get(MyEvents.event_1));
-		assertNull(listener2.counts.get(MyEvents.event_0));
-		assertNull(listener2.counts.get(MyEvents.event_1));
-	}
+    /**
+     * Register and unregister listener, and verify it
+     * is not called when the event is emitted
+     */
+    @Test
+    public void off_simple() {
+        MyEmitter emitter = new MyEmitter();
+        CountingListener listener = new CountingListener();
+        emitter.on(listener);
+        emitter.off(listener);
+        emitter.emit(MyEvents.event_0, "on_multiple_0");
+        emitter.emit(MyEvents.event_1, "on_multiple_1");
+        assertNull(listener.counts.get(MyEvents.event_0));
+        assertNull(listener.counts.get(MyEvents.event_1));
+    }
 
-	/**
-	 * Register a listener for a specific event, and verify it is called
-	 * only when that event is emitted
-	 */
-	@Test
-	public void on_event_simple() {
-		MyEmitter emitter = new MyEmitter();
-		CountingListener listener = new CountingListener();
-		emitter.on(MyEvents.event_0, listener);
-		emitter.emit(MyEvents.event_0, "on_event_simple_0");
-		emitter.emit(MyEvents.event_0, "on_event_simple_0");
-		emitter.emit(MyEvents.event_1, "on_event_simple_1");
-		assertEquals(listener.counts.get(MyEvents.event_0), Integer.valueOf(2));
-		assertNull(listener.counts.get(MyEvents.event_1));
-	}
+    /**
+     * Register and unregister multiple listeners, and verify they
+     * are not called when the event is emitted
+     */
+    @Test
+    public void off_all() {
+        MyEmitter emitter = new MyEmitter();
+        CountingListener listener1 = new CountingListener();
+        CountingListener listener2 = new CountingListener();
+        emitter.on(listener1);
+        emitter.on(listener2);
+        emitter.off();
+        emitter.emit(MyEvents.event_0, "on_multiple_0");
+        emitter.emit(MyEvents.event_1, "on_multiple_1");
+        assertNull(listener1.counts.get(MyEvents.event_0));
+        assertNull(listener1.counts.get(MyEvents.event_1));
+        assertNull(listener2.counts.get(MyEvents.event_0));
+        assertNull(listener2.counts.get(MyEvents.event_1));
+    }
 
-	/**
-	 * Register a listener for a specific event, and verify
-	 * it is no longer called after it has been removed
-	 */
-	@Test
-	public void off_event_simple() {
-		MyEmitter emitter = new MyEmitter();
-		CountingListener listener = new CountingListener();
-		emitter.on(MyEvents.event_0, listener);
-		emitter.emit(MyEvents.event_0, "off_event_simple_0");
-		emitter.emit(MyEvents.event_1, "off_event_simple_1");
-		emitter.off(MyEvents.event_0, listener);
-		emitter.emit(MyEvents.event_0, "off_event_simple_0");
-		assertEquals(listener.counts.get(MyEvents.event_0), Integer.valueOf(1));
-		assertNull(listener.counts.get(MyEvents.event_1));
-	}
+    /**
+     * Register a listener for a specific event, and verify it is called
+     * only when that event is emitted
+     */
+    @Test
+    public void on_event_simple() {
+        MyEmitter emitter = new MyEmitter();
+        CountingListener listener = new CountingListener();
+        emitter.on(MyEvents.event_0, listener);
+        emitter.emit(MyEvents.event_0, "on_event_simple_0");
+        emitter.emit(MyEvents.event_0, "on_event_simple_0");
+        emitter.emit(MyEvents.event_1, "on_event_simple_1");
+        assertEquals(listener.counts.get(MyEvents.event_0), Integer.valueOf(2));
+        assertNull(listener.counts.get(MyEvents.event_1));
+    }
 
-	/**
-	 * Register a "once" listener for a specific event, and
-	 * verify it is called only once when that event is emitted
-	 */
-	@Test
-	public void once_event_simple() {
-		MyEmitter emitter = new MyEmitter();
-		CountingListener listener = new CountingListener();
-		emitter.once(MyEvents.event_0, listener);
-		emitter.emit(MyEvents.event_0, "once_event_simple_0");
-		emitter.emit(MyEvents.event_0, "once_event_simple_0");
-		emitter.emit(MyEvents.event_1, "once_event_simple_1");
-		assertEquals(listener.counts.get(MyEvents.event_0), Integer.valueOf(1));
-		assertNull(listener.counts.get(MyEvents.event_1));
-	}
+    /**
+     * Register a listener for a specific event, and verify
+     * it is no longer called after it has been removed
+     */
+    @Test
+    public void off_event_simple() {
+        MyEmitter emitter = new MyEmitter();
+        CountingListener listener = new CountingListener();
+        emitter.on(MyEvents.event_0, listener);
+        emitter.emit(MyEvents.event_0, "off_event_simple_0");
+        emitter.emit(MyEvents.event_1, "off_event_simple_1");
+        emitter.off(MyEvents.event_0, listener);
+        emitter.emit(MyEvents.event_0, "off_event_simple_0");
+        assertEquals(listener.counts.get(MyEvents.event_0), Integer.valueOf(1));
+        assertNull(listener.counts.get(MyEvents.event_1));
+    }
 
-	/**
-	 * Register a "once" listener for a specific event, then
-	 * remove it, and verify it is not called when that event is emitted
-	 */
-	@Test
-	public void once_off_event_simple() {
-		MyEmitter emitter = new MyEmitter();
-		CountingListener listener = new CountingListener();
-		emitter.once(MyEvents.event_0, listener);
-		emitter.emit(MyEvents.event_1, "once_event_simple_1");
-		emitter.off(MyEvents.event_0, listener);
-		emitter.emit(MyEvents.event_0, "once_event_simple_0");
-		assertNull(listener.counts.get(MyEvents.event_0));
-		assertNull(listener.counts.get(MyEvents.event_1));
-	}
+    /**
+     * Register a "once" listener for a specific event, and
+     * verify it is called only once when that event is emitted
+     */
+    @Test
+    public void once_event_simple() {
+        MyEmitter emitter = new MyEmitter();
+        CountingListener listener = new CountingListener();
+        emitter.once(MyEvents.event_0, listener);
+        emitter.emit(MyEvents.event_0, "once_event_simple_0");
+        emitter.emit(MyEvents.event_0, "once_event_simple_0");
+        emitter.emit(MyEvents.event_1, "once_event_simple_1");
+        assertEquals(listener.counts.get(MyEvents.event_0), Integer.valueOf(1));
+        assertNull(listener.counts.get(MyEvents.event_1));
+    }
+
+    /**
+     * Register a "once" listener for a specific event, then
+     * remove it, and verify it is not called when that event is emitted
+     */
+    @Test
+    public void once_off_event_simple() {
+        MyEmitter emitter = new MyEmitter();
+        CountingListener listener = new CountingListener();
+        emitter.once(MyEvents.event_0, listener);
+        emitter.emit(MyEvents.event_1, "once_event_simple_1");
+        emitter.off(MyEvents.event_0, listener);
+        emitter.emit(MyEvents.event_0, "once_event_simple_0");
+        assertNull(listener.counts.get(MyEvents.event_0));
+        assertNull(listener.counts.get(MyEvents.event_1));
+    }
+
+
+    @Test
+    public void once_off_event() {
+        MyEmitter emitter = new MyEmitter();
+        CountingListener listener = new CountingListener();
+        emitter.once(MyEvents.event_0, listener);
+        emitter.emit(MyEvents.event_0, "once_event_simple_1");
+        emitter.off(MyEvents.event_0, listener);
+        emitter.emit(MyEvents.event_0, "once_event_simple_0");
+        assertEquals((int) listener.counts.get(MyEvents.event_0), 1);
+
+    }
 
     @Test
     public void test_listener_in_listener() {
