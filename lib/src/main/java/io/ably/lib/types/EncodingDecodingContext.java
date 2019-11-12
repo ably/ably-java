9429765ddae424584f67d106d6f10abff4e32f85
Tsviatko Yovtchev <tsviatko@codemerx.com>
2019-11-12 12:50:31 +0200

    Exposing EncodingDecodingContext's constructor enabling codec testing

--- lib/src/main/java/io/ably/lib/types/EncodingDecodingContext.java
+++ lib/src/main/java/io/ably/lib/types/EncodingDecodingContext.java
@@ -3,7 +3,7 @@ package io.ably.lib.types;
 public class EncodingDecodingContext extends BaseEncodingDecodingContext {
 	public final String encoding;
 
-	EncodingDecodingContext(ChannelOptions options, String encoding, Object baseEncodedPreviousPayload){
+	public EncodingDecodingContext(ChannelOptions options, String encoding, Object baseEncodedPreviousPayload){
 		super(options, baseEncodedPreviousPayload);
 		this.encoding = encoding;
 	}
