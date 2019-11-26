69e39e77f58197f5f4b0d660f6947e85c90b14fe
Quintin Willison <quintin@ably.io>
2019-11-26 13:47:53 +0000

    Remove unnecessary exception catch from test.

--- lib/src/test/java/io/ably/lib/test/realtime/RealtimeCryptoMessageTest.java
+++ lib/src/test/java/io/ably/lib/test/realtime/RealtimeCryptoMessageTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 import javax.crypto.spec.IvParameterSpec;
 
@@ -23,162 +24,142 @@ public class RealtimeCryptoMessageTest extends ParameterizedTest {
 	private static final String testDataFile256 = "ably-common/test-resources/crypto-data-256.json";
 
 	@Test
-	public void encrypt_message_128() throws IOException, AblyException {
+	public void encrypt_message_128() throws IOException, AblyException, NoSuchAlgorithmException {
 		final CryptoTestData testData = (CryptoTestData)Setup.loadJson(testDataFile128, CryptoTestData.class);
-		try {
-			byte[] key = Base64Coder.decode(testData.key);
-			byte[] iv = Base64Coder.decode(testData.iv);
-			String algorithm = testData.algorithm;
+		byte[] key = Base64Coder.decode(testData.key);
+		byte[] iv = Base64Coder.decode(testData.iv);
+		String algorithm = testData.algorithm;
 
-			final CipherParams params = Crypto.getParams(algorithm, key);
-			params.ivSpec = new IvParameterSpec(iv);
+		final CipherParams params = Crypto.getParams(algorithm, key);
+		params.ivSpec = new IvParameterSpec(iv);
 
-			CryptoTestItem[] items = testData.items;
-			for(int i = 0; i < items.length; i++) {
-				CryptoTestItem item = items[i];
+		CryptoTestItem[] items = testData.items;
+		for(int i = 0; i < items.length; i++) {
+			CryptoTestItem item = items[i];
 
-				/* read messages from test data */
-				Message testMessage = item.encoded;
-				Message encryptedMessage = item.encrypted;
+			/* read messages from test data */
+			Message testMessage = item.encoded;
+			Message encryptedMessage = item.encrypted;
 
-				/* decode (ie remove any base64 encoding) */
-				testMessage.decode(null);
-				try {
-					encryptedMessage.decode(null);
-				} catch (MessageDecodeException e) {}
+			/* decode (ie remove any base64 encoding) */
+			testMessage.decode(null);
+			try {
+				encryptedMessage.decode(null);
+			} catch (MessageDecodeException e) {}
 
-				/* reset channel cipher, to ensure it uses the given iv */
-				ChannelOptions options = new ChannelOptions() {{encrypted = true; cipherParams = params;}};
+			/* reset channel cipher, to ensure it uses the given iv */
+			ChannelOptions options = new ChannelOptions() {{encrypted = true; cipherParams = params;}};
 
-				/* encrypt plaintext message; encode() also to handle data that is not already string or buffer */
-				testMessage.encode(options);
+			/* encrypt plaintext message; encode() also to handle data that is not already string or buffer */
+			testMessage.encode(options);
 
-				/* compare */
-				assertTrue(Helpers.compareMessage(testMessage, encryptedMessage));
-			}
-		} catch (java.security.NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			fail("Unexpected Algorithm exception");
+			/* compare */
+			assertTrue(Helpers.compareMessage(testMessage, encryptedMessage));
 		}
 	}
 
 	@Test
-	public void encrypt_message_256() throws IOException, AblyException {
+	public void encrypt_message_256() throws IOException, AblyException, NoSuchAlgorithmException {
 		final CryptoTestData testData = (CryptoTestData)Setup.loadJson(testDataFile256, CryptoTestData.class);
-		try {
-			byte[] key = Base64Coder.decode(testData.key);
-			byte[] iv = Base64Coder.decode(testData.iv);
-			String algorithm = testData.algorithm;
+		byte[] key = Base64Coder.decode(testData.key);
+		byte[] iv = Base64Coder.decode(testData.iv);
+		String algorithm = testData.algorithm;
 
-			final CipherParams params = Crypto.getParams(algorithm, key);
-			params.ivSpec = new IvParameterSpec(iv);
+		final CipherParams params = Crypto.getParams(algorithm, key);
+		params.ivSpec = new IvParameterSpec(iv);
 
-			CryptoTestItem[] items = testData.items;
-			for(int i = 0; i < items.length; i++) {
-				CryptoTestItem item = items[i];
+		CryptoTestItem[] items = testData.items;
+		for(int i = 0; i < items.length; i++) {
+			CryptoTestItem item = items[i];
 
-				/* read messages from test data */
-				Message testMessage = item.encoded;
-				Message encryptedMessage = item.encrypted;
+			/* read messages from test data */
+			Message testMessage = item.encoded;
+			Message encryptedMessage = item.encrypted;
 
-				/* decode (ie remove any base64 encoding) */
-				testMessage.decode(null);
-				try {
-					encryptedMessage.decode(null);
-				} catch (MessageDecodeException e) {}
+			/* decode (ie remove any base64 encoding) */
+			testMessage.decode(null);
+			try {
+				encryptedMessage.decode(null);
+			} catch (MessageDecodeException e) {}
 
-				/* reset channel cipher, to ensure it uses the given iv */
-				ChannelOptions options = new ChannelOptions() {{encrypted = true; cipherParams = params;}};
+			/* reset channel cipher, to ensure it uses the given iv */
+			ChannelOptions options = new ChannelOptions() {{encrypted = true; cipherParams = params;}};
 
-				/* encrypt plaintext message; encode() also to handle data that is not already string or buffer */
-				testMessage.encode(options);
+			/* encrypt plaintext message; encode() also to handle data that is not already string or buffer */
+			testMessage.encode(options);
 
-				/* compare */
-				assertTrue(Helpers.compareMessage(testMessage, encryptedMessage));
-			}
-		} catch (java.security.NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			fail("Unexpected Algorithm exception");
+			/* compare */
+			assertTrue(Helpers.compareMessage(testMessage, encryptedMessage));
 		}
 	}
 
 	@Test
-	public void decrypt_message_128() throws IOException, MessageDecodeException {
+	public void decrypt_message_128() throws IOException, MessageDecodeException, NoSuchAlgorithmException {
 		final CryptoTestData testData = (CryptoTestData)Setup.loadJson(testDataFile128, CryptoTestData.class);
-		try {
-			byte[] key = Base64Coder.decode(testData.key);
-			byte[] iv = Base64Coder.decode(testData.iv);
-			String algorithm = testData.algorithm;
+		byte[] key = Base64Coder.decode(testData.key);
+		byte[] iv = Base64Coder.decode(testData.iv);
+		String algorithm = testData.algorithm;
 
-			final CipherParams params = Crypto.getParams(algorithm, key);
-			params.ivSpec = new IvParameterSpec(iv);
+		final CipherParams params = Crypto.getParams(algorithm, key);
+		params.ivSpec = new IvParameterSpec(iv);
 
-			CryptoTestItem[] items = testData.items;
-			for(int i = 0; i < items.length; i++) {
-				CryptoTestItem item = items[i];
+		CryptoTestItem[] items = testData.items;
+		for(int i = 0; i < items.length; i++) {
+			CryptoTestItem item = items[i];
 
-				/* read messages from test data */
-				Message testMessage = item.encoded;
-				Message encryptedMessage = item.encrypted;
+			/* read messages from test data */
+			Message testMessage = item.encoded;
+			Message encryptedMessage = item.encrypted;
 
-				/* decode (ie remove any base64 encoding) */
-				testMessage.decode(null);
-				try {
-					encryptedMessage.decode(null);
-				} catch (MessageDecodeException e) {}
+			/* decode (ie remove any base64 encoding) */
+			testMessage.decode(null);
+			try {
+				encryptedMessage.decode(null);
+			} catch (MessageDecodeException e) {}
 
-				/* reset channel cipher, to ensure it uses the given iv */
-				ChannelOptions options = new ChannelOptions() {{encrypted = true; cipherParams = params;}};
+			/* reset channel cipher, to ensure it uses the given iv */
+			ChannelOptions options = new ChannelOptions() {{encrypted = true; cipherParams = params;}};
 
-				/* decrypt message; decode() also to handle data that is not already string or buffer */
-				encryptedMessage.decode(options);
+			/* decrypt message; decode() also to handle data that is not already string or buffer */
+			encryptedMessage.decode(options);
 
-				/* compare */
-				assertTrue(Helpers.compareMessage(testMessage, encryptedMessage));
-			}
-		} catch (java.security.NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			fail("Unexpected Algorithm exception");
+			/* compare */
+			assertTrue(Helpers.compareMessage(testMessage, encryptedMessage));
 		}
 	}
 
 	@Test
-	public void decrypt_message_256() throws IOException, MessageDecodeException {
+	public void decrypt_message_256() throws IOException, MessageDecodeException, NoSuchAlgorithmException {
 		final CryptoTestData testData = (CryptoTestData)Setup.loadJson(testDataFile256, CryptoTestData.class);
-		try {
-			byte[] key = Base64Coder.decode(testData.key);
-			byte[] iv = Base64Coder.decode(testData.iv);
-			String algorithm = testData.algorithm;
+		byte[] key = Base64Coder.decode(testData.key);
+		byte[] iv = Base64Coder.decode(testData.iv);
+		String algorithm = testData.algorithm;
 
-			final CipherParams params = Crypto.getParams(algorithm, key);
-			params.ivSpec = new IvParameterSpec(iv);
+		final CipherParams params = Crypto.getParams(algorithm, key);
+		params.ivSpec = new IvParameterSpec(iv);
 
-			CryptoTestItem[] items = testData.items;
-			for(int i = 0; i < items.length; i++) {
-				CryptoTestItem item = items[i];
+		CryptoTestItem[] items = testData.items;
+		for(int i = 0; i < items.length; i++) {
+			CryptoTestItem item = items[i];
 
-				/* read messages from test data */
-				Message testMessage = item.encoded;
-				Message encryptedMessage = item.encrypted;
+			/* read messages from test data */
+			Message testMessage = item.encoded;
+			Message encryptedMessage = item.encrypted;
 
-				/* decode (ie remove any base64 encoding) */
-				testMessage.decode(null);
-				try {
-					encryptedMessage.decode(null);
-				} catch (MessageDecodeException e) {}
+			/* decode (ie remove any base64 encoding) */
+			testMessage.decode(null);
+			try {
+				encryptedMessage.decode(null);
+			} catch (MessageDecodeException e) {}
 
-				/* reset channel cipher, to ensure it uses the given iv */
-				ChannelOptions options = new ChannelOptions() {{encrypted = true; cipherParams = params;}};
+			/* reset channel cipher, to ensure it uses the given iv */
+			ChannelOptions options = new ChannelOptions() {{encrypted = true; cipherParams = params;}};
 
-				/* decrypt message; decode() also to handle data that is not already string or buffer */
-				encryptedMessage.decode(options);
+			/* decrypt message; decode() also to handle data that is not already string or buffer */
+			encryptedMessage.decode(options);
 
-				/* compare */
-				assertTrue(Helpers.compareMessage(testMessage, encryptedMessage));
-			}
-		} catch (java.security.NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			fail("Unexpected Algorithm exception");
+			/* compare */
+			assertTrue(Helpers.compareMessage(testMessage, encryptedMessage));
 		}
 	}
 
