c4d0e8121eda3cce8df2cd89232ad0f86bb2fb97
Quintin Willison <quintin@ably.io>
2019-11-29 18:02:33 +0000

    Add encrypted fixture generator to the Java unit tests.

--- lib/src/test/java/io/ably/lib/util/CryptoTest.java
+++ lib/src/test/java/io/ably/lib/util/CryptoTest.java
@@ -1,15 +1,21 @@
 package io.ably.lib.util;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 import org.junit.Test;
 
+import com.google.gson.stream.JsonWriter;
+
 import io.ably.lib.types.AblyException;
 import io.ably.lib.types.ChannelOptions;
+import io.ably.lib.util.Crypto.ChannelCipher;
 import io.ably.lib.util.Crypto.CipherParams;
 
 public class CryptoTest {
@@ -46,10 +52,10 @@ public class CryptoTest {
 		);
 
 		byte[] plaintext = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-		Crypto.ChannelCipher channelCipher1 = Crypto.getCipher(new ChannelOptions() {{ encrypted=true; cipherParams=params1; }});
-		Crypto.ChannelCipher channelCipher2 = Crypto.getCipher(new ChannelOptions() {{ encrypted=true; cipherParams=params2; }});
-		Crypto.ChannelCipher channelCipher3 = Crypto.getCipher(new ChannelOptions() {{ encrypted=true; cipherParams=params3; }});
-		Crypto.ChannelCipher channelCipher4 = Crypto.getCipher(new ChannelOptions() {{ encrypted=true; cipherParams=params4; }});
+		ChannelCipher channelCipher1 = Crypto.getCipher(new ChannelOptions() {{ encrypted=true; cipherParams=params1; }});
+		ChannelCipher channelCipher2 = Crypto.getCipher(new ChannelOptions() {{ encrypted=true; cipherParams=params2; }});
+		ChannelCipher channelCipher3 = Crypto.getCipher(new ChannelOptions() {{ encrypted=true; cipherParams=params3; }});
+		ChannelCipher channelCipher4 = Crypto.getCipher(new ChannelOptions() {{ encrypted=true; cipherParams=params4; }});
 
 		byte[] ciphertext1 = channelCipher1.encrypt(plaintext);
 		byte[] ciphertext2 = channelCipher2.encrypt(plaintext);
@@ -62,5 +68,109 @@ public class CryptoTest {
 		    Arrays.equals(ciphertext1, ciphertext3) &&
 		    Arrays.equals(ciphertext1, ciphertext4)
 		);
+	}
+
+	/**
+ 	 * Test encryption using a 256 bit key and varying lengths of data.
+	 *
+ 	 * The key, IV and message data are the same for every test run so that the
+ 	 * encrypted data may be exported from the console output for consumption by tests
+ 	 * run on other platforms.
+ 	 *
+	 * Equivalent to the following in ably-cocoa:
+	 * testEncryptAndDecrypt in Spec/CryptoTest.m
+	 * @throws IOException 
+	 */
+	@Test
+	public void encryptAndDecrypt() throws NoSuchAlgorithmException, AblyException, IOException {
+		final byte[] key = {
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+			21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+			31, 32,
+		};
+		final byte[] iv = {
+			16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
+		};
+		final CipherParams params = Crypto.getDefaultParams(key, iv);
+
+		// Prepare message data.
+		final int maxLength = 70;
+		final byte[] message = new byte[maxLength];
+		for (byte value=1; value<=maxLength; value++) {
+			message[value - 1] = value;
+		}
+
+		final StringWriter target = new StringWriter();
+		final JsonWriter writer = new JsonWriter(target);
+		writer.setIndent("  ");
+		writer.beginObject();
+
+		writer.name("algorithm");
+		writer.value("aes");
+
+		writer.name("mode");
+		writer.value("cbc");
+		
+		writer.name("keyLength");
+		writer.value(256);
+		
+		writer.name("key");
+		writer.value(Base64Coder.encodeToString(key));
+		
+		writer.name("iv");
+		writer.value(Base64Coder.encodeToString(iv));
+
+		// Perform encrypt and decrypt on message data trimmed at all lengths up
+		// to and including maxLength.
+		writer.name("items");
+		writer.beginArray();
+		for (int i=1; i<=maxLength; i++) {
+			// We need to create a new ChannelCipher for each message we encode,
+			// so that our IV gets used (being start of CBC chain).
+			final ChannelCipher cipher = Crypto.getCipher(new ChannelOptions() {{ encrypted=true; cipherParams=params; }});
+
+			// Encrypt i bytes from the start of the message data.
+			final byte[] encoded = Arrays.copyOfRange(message, 0, i);
+			final byte[] encrypted = cipher.encrypt(encoded);
+
+			// Add encryption result to results in format ready for fixture.
+			writeResult(writer, encoded, encrypted);
+
+			// Decrypt the encrypted data and verify the result is the same as what
+			// we submitted for encryption.
+			final byte[] verify = cipher.decrypt(encrypted);
+			assertArrayEquals(verify, encoded);
+		}
+		writer.endArray();
+
+		writer.endObject();
+
+		System.out.println("Fixture JSON for test-resources:\n" + target.toString());
+	}
+
+	private void writeResult(final JsonWriter writer, final byte[] encoded, final byte[] encrypted) throws IOException {
+		writer.beginObject();
+
+		writer.name("encoded");
+		writeData(writer, encoded, null);
+
+		writer.name("encrypted");
+		writeData(writer, encoded, "cipher+aes-256-cbc");
+
+		writer.endObject();
+	}
+
+	private static final String BASE64 = "base64";
+	private void writeData(final JsonWriter writer, final byte[] data, final String encoding) throws IOException {
+		writer.beginObject();
+
+		writer.name("data");
+		writer.value(Base64Coder.encodeToString(data));
+
+		writer.name("encoding");
+		writer.value(null == encoding ? BASE64 : encoding + "/" + BASE64);
+
+		writer.endObject();
 	}
 }
