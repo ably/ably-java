43a9a5103c0d94417a62ecee51a6b4a518af97d7
Tsviatko Yovtchev <tsviatko@codemerx.com>
2020-01-06 21:33:18 +0200

    Adding delta failure recovery

--- lib/src/test/java/io/ably/lib/test/realtime/RealtimeDeltaDecoderTest.java
+++ lib/src/test/java/io/ably/lib/test/realtime/RealtimeDeltaDecoderTest.java
@@ -62,4 +62,53 @@ public class RealtimeDeltaDecoderTest extends ParameterizedTest {
 				ably.close();
 		}
 	}
+
+//	@Test
+//	public void delta_decode_failure_recovery() {
+//		AblyRealtime ably = null;
+//		String testName = "delta_decode_failure_recovery";
+//		try {
+//			ClientOptions opts = createOptions(testVars.keys[0].keyStr);
+//			MonitoredCodec monitoredCodec = new MonitoredCodec(new FailingDeltaCodec());
+//			opts.Codecs.put(PluginType.vcdiffDecoder, monitoredCodec);
+//			ably = new AblyRealtime(opts);
+//
+//			/* create a channel */
+//			final Channel channel = ably.channels.get("[?delta=vcdiff]" + testName);
+//
+//			/* attach */
+//			channel.attach();
+//			(new ChannelWaiter(channel)).waitFor(ChannelState.attached);
+//			assertEquals("Verify attached state reached", channel.state, ChannelState.attached);
+//
+//			/* subscribe */
+//			MessageWaiter messageWaiter = new MessageWaiter(channel);
+//
+//			/* publish to the channel */
+//			for (int i = 0; i < testData.length; i++) {
+//				channel.publish(Integer.toString(i), testData[i]);
+//			}
+//
+//			/* wait for the messages */
+//			messageWaiter.waitFor(testData.length);
+//			for (Message message: messageWaiter.receivedMessages) {
+//				assertEquals("Verify message data", testData[Integer.parseInt(message.name)], message.data);
+//			}
+//			assertEquals("Verify number of calls to the codec", testData.length - 1, monitoredCodec.numberOfCalls);
+//		} catch(Exception e) {
+//			fail(testName + ": Unexpected exception " + e.getMessage());
+//			e.printStackTrace();
+//		} finally {
+//			if(ably != null)
+//				ably.close();
+//		}
+//	}
+//}
+//
+//    class FailingDeltaCodec implements VCDiffPluggableCodec {
+//		@Override
+//		public byte[] decode(byte[] delta, byte[] base) throws MessageDecodeException {
+//			throw MessageDecodeException.fromThrowableAndErrorInfo(null, new ErrorInfo("Delta decode failed", 400, 40018));
+//		}
+//	}
 }
