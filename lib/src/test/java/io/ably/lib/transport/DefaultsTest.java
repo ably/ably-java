090eba21c2d4148dac0fd3c514c5798c41a4037e
Quintin Willison <quintin@ably.io>
2020-07-06 09:18:05 +0100

    Introduce a pure unit testing 'suite' for the ARTDefault class.
    Small for now as it's targeting the issue I'm working on right now, however this will grow.

---
+++ lib/src/test/java/io/ably/lib/transport/DefaultsTest.java
@@ -0,0 +1,12 @@
+package io.ably.lib.transport;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DefaultsTest {
+	@Test
+	public void versions() {
+		assertEquals("1.2", Defaults.ABLY_VERSION);
+	}
+}
