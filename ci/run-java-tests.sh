f4f5aa998bb6f2baec298a4860ddee83f71a6130
Quintin Willison <q@qwuk.net>
2021-01-21 22:15:43 +0000

    Remove Travis.

--- ci/run-java-tests.sh
+++
@@ -1,8 +0,0 @@
-# We unset this, otherwise gradlew picks up settings also from the android "context", like here: https://travis-ci.org/ably/ably-java/jobs/353969106#L1980
-unset ANDROID_HOME
-
-ret=0
-./gradlew runUnitTests || ret=1
-./gradlew java:testRealtimeSuite || ret=1
-./gradlew java:testRestSuite || ret=1
-exit $ret
