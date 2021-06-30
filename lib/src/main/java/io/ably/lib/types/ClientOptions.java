343095ea30e0f21624a41b2d4615a19016e75ed5
Kacper Kluka <k.kluka@nomtek.com>
2021-06-30 12:47:24 +0200

    Update ClientOptions.agents javadoc

--- lib/src/main/java/io/ably/lib/types/ClientOptions.java
+++ lib/src/main/java/io/ably/lib/types/ClientOptions.java
@@ -198,6 +198,11 @@ public class ClientOptions extends AuthOptions {
 
     /**
      * Map of agents that will be appended to the agent header.
+     *
+     * This should only be used by Ably-authored SDKs.
+     * If you need to use this then you have to add the agent to the agents.json file:
+     * https://github.com/ably/ably-common/blob/main/protocol/agents.json
+     *
      * The keys represent agent names and its corresponding values represent agent versions.
      * Agent versions are optional, if you don't want to specify it pass `null` as the map entry value.
      */
