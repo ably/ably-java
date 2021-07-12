872859ff99ab1526bd47bac1df3919593ee479c4
Martin Morek <martin.morek@gmail.com>
2021-07-12 17:15:07 +0200

    Added missing documentation

--- lib/src/main/java/io/ably/lib/push/Storage.java
+++ lib/src/main/java/io/ably/lib/push/Storage.java
@@ -4,17 +4,42 @@ import java.lang.reflect.Field;
 
 /**
  * Interface for an entity that supplies key value store
- * - methods getString and getInt have to return default value if requested key is not found
  */
 public interface Storage {
 
+    /**
+     * Insert string value in to storage
+     * @param key name under which value is stored
+     * @param value stored string value
+     */
     void putString(String key, String value);
 
+    /**
+     * Insert integer value in to storage
+     * @param key name after which value is stored
+     * @param value stored integer value
+     */
     void putInt(String key, int value);
 
+    /**
+     * Returns string value based on key from storage
+     * @param key name under value is stored
+     * @param defaultValue value which is returned if key is not found
+     * @return value stored under key or default value if key is not found
+     */
     String getString(String key, String defaultValue);
 
+    /**
+     * Returns integer value based on key from storage
+     * @param key name under value is stored
+     * @param defaultValue value which is returned if key is not found
+     * @return value stored under key or default value if key is not found
+     */
     int getInt(String key, int defaultValue);
 
+    /**
+     * Removes fields from storage
+     * @param fields array of keys which values should be removed from storage
+     */
     void clear(Field[] fields);
 }
