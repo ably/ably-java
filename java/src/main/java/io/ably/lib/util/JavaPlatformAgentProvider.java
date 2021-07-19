2e870ac7a94225ada965fe5d6e962325d8b493f0
Kacper Kluka <k.kluka@nomtek.com>
2021-07-19 08:52:09 +0200

    Add JRE for the Java platform agent

--- java/src/main/java/io/ably/lib/util/JavaPlatformAgentProvider.java
+++ java/src/main/java/io/ably/lib/util/JavaPlatformAgentProvider.java
@@ -3,6 +3,6 @@ package io.ably.lib.util;
 public class JavaPlatformAgentProvider implements PlatformAgentProvider {
     @Override
     public String createPlatformAgent() {
-        return null;
+        return "jre" + AgentHeaderCreator.AGENT_DIVIDER + System.getProperty("java.version");
     }
 }
