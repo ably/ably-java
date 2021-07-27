05f2999917016aa9d297b5d934a31e86ef5357cc
Martin Morek <martin.morek@gmail.com>
2021-07-27 17:28:01 +0200

    Added unit tests

--- lib/src/main/java/io/ably/lib/util/ParamsUtils.java
+++ lib/src/main/java/io/ably/lib/util/ParamsUtils.java
@@ -5,6 +5,13 @@ import io.ably.lib.types.Param;
 
 public class ParamsUtils {
 
+    /**
+     * Produce either new or extend provided array of parameters based on values in Client options
+     *
+     * @param params Array of already set parameters
+     * @param options Client options
+     * @return Array of parameters extended of parameters based on values in client options
+     */
     public static Param[] enrichParams(Param[] params, ClientOptions options) {
         if (options.pushFullWait) {
             params = Param.push(params, "fullWait", "true");
