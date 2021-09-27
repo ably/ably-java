8499c51be53c8d31d15670fc737a525fec942366
Martin <martin.morek@gmail.com>
2021-09-27 15:39:08 +0200

    Added missing import

--- lib/src/test/java/io/ably/lib/test/realtime/RealtimeAuthTest.java
+++ lib/src/test/java/io/ably/lib/test/realtime/RealtimeAuthTest.java
@@ -28,6 +28,7 @@ import org.junit.Test;
 import org.junit.rules.Timeout;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
