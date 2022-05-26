f0f5a8ad981081a37863d6e2baff505d7b7b9515
Kacper Kluka <k.kluka@nomtek.com>
2022-05-26 11:01:12 +0200

    Add commentary around platform classes visibility

--- android/src/main/java/io/ably/lib/platform/AndroidPlatform.java
+++ android/src/main/java/io/ably/lib/platform/AndroidPlatform.java
@@ -7,6 +7,11 @@ import io.ably.lib.types.AblyException;
 import io.ably.lib.types.ErrorInfo;
 import io.ably.lib.util.Log;
 
+/**
+ * An internal class with platform specific logic.
+ * Even though it's internal, its visibility modifier is public because it's referenced by two other packages in this module (one for REST, one for Realtime).
+ * This is something we would like to solve in future but solving that is not in the scope of the work currently being done.
+ */
 public class AndroidPlatform implements PlatformBase {
     public static final String name = "android";
 
--- java/src/main/java/io/ably/lib/platform/JavaPlatform.java
+++ java/src/main/java/io/ably/lib/platform/JavaPlatform.java
@@ -2,6 +2,11 @@ package io.ably.lib.platform;
 
 import io.ably.lib.transport.NetworkConnectivity;
 
+/**
+ * An internal class with platform specific logic.
+ * Even though it's internal, its visibility modifier is public because it's referenced by two other packages in this module (one for REST, one for Realtime).
+ * This is something we would like to solve in future but solving that is not in the scope of the work currently being done.
+ */
 public class JavaPlatform implements PlatformBase {
     public static final String name = "java";
 
