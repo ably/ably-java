98ced2f1d5e017198771540423b74c5fcadf8ce5
QSD_igor <info@qsdbih.com>
2022-07-25 13:43:45 +0200

    Remove Type from RegistrationToken as only FCM is supported, Remove deprecated test RegistrationTokenTypeTest Refactor token type usage across LocalDevice as FCM only exist Refactor generateSecret method for better try

--- android/src/main/java/io/ably/lib/push/LocalDevice.java
+++ android/src/main/java/io/ably/lib/push/LocalDevice.java
@@ -2,17 +2,19 @@ package io.ably.lib.push;
 
 import android.content.Context;
 import android.content.res.Configuration;
+
 import com.google.gson.JsonObject;
-import io.ably.lib.rest.DeviceDetails;
-import io.ably.lib.types.Param;
-import io.ably.lib.types.RegistrationToken;
-import io.ably.lib.util.Base64Coder;
-import io.ably.lib.util.Log;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.UUID;
+
+import io.ably.lib.rest.DeviceDetails;
+import io.ably.lib.types.Param;
+import io.ably.lib.types.RegistrationToken;
+import io.ably.lib.util.Base64Coder;
+import io.ably.lib.util.Log;
 
 public class LocalDevice extends DeviceDetails {
     public String deviceSecret;
@@ -45,7 +47,7 @@ public class LocalDevice extends DeviceDetails {
         /* Spec: RSH8a */
         String id = storage.get(SharedPrefKeys.DEVICE_ID, null);
         this.id = id;
-        if(id != null) {
+        if (id != null) {
             Log.v(TAG, "loadPersisted(): existing deviceId found; id: " + id);
             deviceSecret = storage.get(SharedPrefKeys.DEVICE_SECRET, null);
         } else {
@@ -54,32 +56,26 @@ public class LocalDevice extends DeviceDetails {
         this.clientId = storage.get(SharedPrefKeys.CLIENT_ID, null);
         this.deviceIdentityToken = storage.get(SharedPrefKeys.DEVICE_TOKEN, null);
 
-        final int ordinalTypeValue = storage.get(SharedPrefKeys.TOKEN_TYPE, -1);
-        if (ordinalTypeValue != RegistrationToken.TOKEN_TYPE_ORDINAL_VALUE_FCM) {
-            Log.e(TAG, "Cannot load persisted token because ordinal does not indicate FCM (it's " + ordinalTypeValue + ").");
-            return;
-        }
-
-        RegistrationToken token = null;
-        String tokenString = storage.get(SharedPrefKeys.TOKEN, null);
-        Log.d(TAG, "loadPersisted(): FCM token string = " + tokenString);
-        if(tokenString != null) {
-            token = new RegistrationToken(tokenString);
+        String tokenString = storage.get(SharedPrefKeys.REGISTRATION_TOKEN, null);
+        if (tokenString != null) {
+            Log.d(TAG, "loadPersisted(): FCM token string = " + tokenString);
+            RegistrationToken token = new RegistrationToken(tokenString);
             setRegistrationToken(token);
+        } else {
+            Log.v(TAG, "loadPersisted(): existing FCM token not found.");
         }
     }
 
     RegistrationToken getRegistrationToken() {
         JsonObject recipient = push.recipient;
-        if(recipient == null) {
+        if (recipient == null) {
             Log.v(TAG, "getRegistrationToken(): returning null because push.recipient is null");
             return null;
         }
 
-        // GCM is no longer active, therefore we only support FCM.
         final String tokenType = recipient.get("transportType").getAsString();
-        if (tokenType != RegistrationToken.TOKEN_TYPE_STRING_VALUE_FCM) {
-            // Presumably GCM, which has been deactivated by Google.
+        if (!RegistrationToken.TOKEN_TYPE_FCM.equals(tokenType)) {
+            //FCM is only supported as we do not support other types.
             Log.e(TAG, "getRegistrationToken(): returning null because push.recipient.transportType is not FCM (it's \"" + tokenType + "\")");
             return null;
         }
@@ -91,7 +87,7 @@ public class LocalDevice extends DeviceDetails {
     private void setRegistrationToken(RegistrationToken token) {
         Log.v(TAG, "setRegistrationToken(): token=" + token);
         push.recipient = new JsonObject();
-        push.recipient.addProperty("transportType", token.type.toName());
+        push.recipient.addProperty("transportType", RegistrationToken.TOKEN_TYPE_FCM);
         push.recipient.addProperty("registrationToken", token.token);
     }
 
@@ -103,8 +99,7 @@ public class LocalDevice extends DeviceDetails {
     void setAndPersistRegistrationToken(RegistrationToken token) {
         Log.v(TAG, "setAndPersistRegistrationToken(): token=" + token);
         setRegistrationToken(token);
-        storage.put(SharedPrefKeys.TOKEN_TYPE, token.type.ordinal());
-        storage.put(SharedPrefKeys.TOKEN, token.token);
+        storage.put(SharedPrefKeys.REGISTRATION_TOKEN, token.token);
     }
 
     void setClientId(String clientId) {
@@ -154,8 +149,8 @@ public class LocalDevice extends DeviceDetails {
 
     private static boolean isTablet(Context context) {
         return (context.getResources().getConfiguration().screenLayout
-                & Configuration.SCREENLAYOUT_SIZE_MASK)
-                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+            & Configuration.SCREENLAYOUT_SIZE_MASK)
+            >= Configuration.SCREENLAYOUT_SIZE_LARGE;
     }
 
     private static class SharedPrefKeys {
@@ -163,17 +158,7 @@ public class LocalDevice extends DeviceDetails {
         static final String CLIENT_ID = "ABLY_CLIENT_ID";
         static final String DEVICE_SECRET = "ABLY_DEVICE_SECRET";
         static final String DEVICE_TOKEN = "ABLY_DEVICE_IDENTITY_TOKEN";
-
-        /**
-         * Historically this value was required because we had two token types, GCM and FCM.
-         * Google have now deactivated GCM, so the only currently supported type is FCM.
-         * The value stored against this key is an integer ordinal, where:
-         * - 0 represents GCM (not supported)
-         * - 1 represents FCM (supported) - defined, for clarity, as {@link RegistrationToken#TOKEN_TYPE_ORDINAL_VALUE_FCM}
-         */
-        static final String TOKEN_TYPE = "ABLY_REGISTRATION_TOKEN_TYPE";
-
-        static final String TOKEN = "ABLY_REGISTRATION_TOKEN";
+        static final String REGISTRATION_TOKEN = "ABLY_REGISTRATION_TOKEN";
     }
 
     private static String generateSecret() {
--- android/src/main/java/io/ably/lib/types/RegistrationToken.java
+++ android/src/main/java/io/ably/lib/types/RegistrationToken.java
@@ -1,83 +1,32 @@
 package io.ably.lib.types;
 
-import java.util.Locale;
+import androidx.annotation.NonNull;
 
 public class RegistrationToken {
-    /**
-     * The type of registration token represented by the value in {@link #token}.
-     * @deprecated As of version 1.2.11. Only FCM is active now as GCM has been deactivated by Google.
-     */
-    @Deprecated
-    public Type type;
-
     /**
      * The token value.
      */
     public String token;
 
     /**
-     * @deprecated As of version 1.2.11.
-     * Use {@link #RegistrationToken(String)} instead, to create an FCM token.
-     * Only FCM is active now as GCM has been deactivated by Google.
-     */
-    @Deprecated
-    public RegistrationToken(Type type, String token) {
-        this.type = type;
-        this.token = token;
-    }
-
-    /**
      * Initializes a newly created RegistrationToken object representing an FCM registration token.
+     *
      * @param token The FCM token value.
      */
     public RegistrationToken(String token) {
-        this.type = Type.FCM;
         this.token = token;
     }
 
     /**
      * The Ably transportType used to categorise an FCM registration token.
      */
-    public static final String TOKEN_TYPE_STRING_VALUE_FCM = "fcm";
+    public static final String TOKEN_TYPE_FCM = "fcm";
 
-    /**
-     * The value persisted to shared preferences on Android to categorise an FCM registration token.
-     */
-    public static final int TOKEN_TYPE_ORDINAL_VALUE_FCM = 1;
-
-    /**
-     * @deprecated As of version 1.2.11. GCM has been deactivated by Google, use FCM instead.
-     */
-    @Deprecated
-    public enum Type {
-        GCM,
-        FCM;
-
-        public static Type fromOrdinal(int i) {
-            try {
-                return Type.values()[i];
-            } catch(Throwable t) {
-                return null;
-            }
-        }
-
-        public static Type fromName(String name) {
-            try {
-                return Type.valueOf(name.toUpperCase(Locale.ROOT));
-            } catch(Throwable t) {
-                return null;
-            }
-        }
-
-        public String toName() {
-            return name().toLowerCase(Locale.ROOT);
-        }
-    }
-
+    @NonNull
     @Override
     public String toString() {
         return "RegistrationToken{" +
-            "type=" + type +
+            "type=" + TOKEN_TYPE_FCM +
             ", token='" + token + '\'' +
             '}';
     }
