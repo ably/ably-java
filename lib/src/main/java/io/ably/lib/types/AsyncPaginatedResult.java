30cb3d6ad2a9377ffaf90e666481a0baf90b90ba
QSD_igor <info@qsd.ba>
2022-09-07 15:58:30 +0200

    Document behaviour of AsyncPaginatedResult

--- lib/src/main/java/io/ably/lib/types/AsyncPaginatedResult.java
+++ lib/src/main/java/io/ably/lib/types/AsyncPaginatedResult.java
@@ -10,18 +10,40 @@ package io.ably.lib.types;
 public interface AsyncPaginatedResult<T> {
 
     /**
-     * Get the contents as an array of component type
+     * Contains the current page of results; for example, an array of {@link Message} or {@link PresenceMessage}
+     * objects for a channel history request.
+     * <p>
+     * Spec: TG3
      */
     T[] items();
 
     /**
-     * Obtain params required to perform the given relative query
+     * Returns a new PaginatedResult for the first page of results.
+     * <p>
+     * Spec: TG5
      */
     void first(Callback<AsyncPaginatedResult<T>> callback);
+    /**
+     * Returns a new PaginatedResult for the current page of results.
+     * <p>
+     * Spec: TG5
+     */
     void current(Callback<AsyncPaginatedResult<T>> callback);
+    /**
+     * Returns a new PaginatedResult for the next page of results.
+     * <p>
+     * Spec: TG5
+     */
     void next(Callback<AsyncPaginatedResult<T>> callback);
 
     boolean hasFirst();
     boolean hasCurrent();
+
+    /**
+     * Returns true if there are more pages available by calling next and returns false if this page is the last page available.
+     * <p>
+     * Spec: TG6
+     * @return Whether or not there are more pages of results.
+     */
     boolean hasNext();
 }
