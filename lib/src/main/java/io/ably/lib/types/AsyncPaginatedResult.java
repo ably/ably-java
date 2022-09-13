3c524a436752d96cbd3c6871e5770f2a0bf940b9
QSD_igor <info@qsd.ba>
2022-09-13 16:21:36 +0200

    Fix documentation behaviour of next() in AsyncPaginatedResult class

--- lib/src/main/java/io/ably/lib/types/AsyncPaginatedResult.java
+++ lib/src/main/java/io/ably/lib/types/AsyncPaginatedResult.java
@@ -30,9 +30,11 @@ public interface AsyncPaginatedResult<T> {
      */
     void current(Callback<AsyncPaginatedResult<T>> callback);
     /**
-     * Returns a new PaginatedResult for the next page of results.
+     * Returns a new PaginatedResult loaded with the next page of results.
+     * If there are no further pages, then null is returned.
      * <p>
-     * Spec: TG5
+     * Spec: TG4
+     * @return A page of results for message and presence history, stats, and REST presence requests.
      */
     void next(Callback<AsyncPaginatedResult<T>> callback);
 
