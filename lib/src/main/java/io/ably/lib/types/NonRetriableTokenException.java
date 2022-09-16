1b82e11e0ffddf9e6d202d9fae59f193786bf0d1
Kacper Kluka <k.kluka@nomtek.com>
2022-09-16 15:51:40 +0200

    Add information about used pattern in the interface documentation

--- lib/src/main/java/io/ably/lib/types/NonRetriableTokenException.java
+++ lib/src/main/java/io/ably/lib/types/NonRetriableTokenException.java
@@ -1,7 +1,7 @@
 package io.ably.lib.types;
 
 /**
- * Implement this interface in your exception class if the token auth operation should not be retried.
+ * Implement this marker interface in your exception class if the token auth operation should not be retried.
  */
 public interface NonRetriableTokenException {
 
