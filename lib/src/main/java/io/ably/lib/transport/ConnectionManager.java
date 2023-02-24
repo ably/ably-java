04434ee4f4ee418dc86e250d4e1d1bcb1a0fafbb
ikbalkaya <ceikbal@gmail.com>
2023-02-24 17:41:20 +0000

    Remove unused import

--- lib/src/main/java/io/ably/lib/transport/ConnectionManager.java
+++ lib/src/main/java/io/ably/lib/transport/ConnectionManager.java
@@ -8,7 +8,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
