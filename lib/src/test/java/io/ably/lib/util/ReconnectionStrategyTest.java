25aaf76c33f7c3027844d11734184e54dc9224e1
sacOO7 <sachinshinde7676@gmail.com>
2023-06-22 13:35:28 +0530

    Reformatted code as per java standard

--- lib/src/test/java/io/ably/lib/util/ReconnectionStrategyTest.java
+++ lib/src/test/java/io/ably/lib/util/ReconnectionStrategyTest.java
@@ -1,7 +1,9 @@
 package io.ably.lib.util;
 
 import org.junit.Test;
+
 import java.util.Arrays;
+
 import static io.ably.lib.test.common.Helpers.assertTimeoutBetween;
 
 public class ReconnectionStrategyTest {
@@ -9,7 +11,7 @@ public class ReconnectionStrategyTest {
     @Test
     public void calculateRetryTimeoutUsingIncrementalBackoffAndJitter() {
 
-        int[] retryAttempts = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        int[] retryAttempts = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         int initialTimeoutValue = 15; // timeout value in seconds
 
         int[] retryTimeouts = Arrays.stream(retryAttempts).map(attempt -> ReconnectionStrategy.getRetryTime(initialTimeoutValue, attempt)).toArray();
@@ -18,8 +20,7 @@ public class ReconnectionStrategyTest {
         assertTimeoutBetween(retryTimeouts[1], 16d, 20d);
         assertTimeoutBetween(retryTimeouts[2], 20d, 25d);
 
-        for (int i = 3; i < retryTimeouts.length; i++)
-        {
+        for (int i = 3; i < retryTimeouts.length; i++) {
             assertTimeoutBetween(retryTimeouts[i], 24d, 30d);
         }
 
@@ -32,19 +33,18 @@ public class ReconnectionStrategyTest {
         }
     }
 
-    static class Bounds {
-        public Bounds(Double lower, Double upper) {
-            this.lower = lower;
-            this.upper = upper;
-        }
-        public Double lower;
-        public Double upper;
-    }
-
-    public Bounds calculateRetryBounds(int retryAttempt, int initialTimeout)
-    {
+    public Bounds calculateRetryBounds(int retryAttempt, int initialTimeout) {
         double upperBound = Math.min((retryAttempt + 2) / 3d, 2d) * initialTimeout;
         double lowerBound = 0.8 * upperBound;
         return new Bounds(lowerBound, upperBound);
+    }
+
+    static class Bounds {
+        Double lower;
+        Double upper;
+        Bounds(Double lower, Double upper) {
+            this.lower = lower;
+            this.upper = upper;
+        }
     }
 }
