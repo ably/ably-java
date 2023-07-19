22aebf6c110714376cc4890c672964b54c38575f
Andy Ford <andy.ford@ably.com>
2023-07-19 21:02:57 +0100

    style: fix style

--- lib/src/test/java/io/ably/lib/realtime/FilteredListenersTest.java
+++ lib/src/test/java/io/ably/lib/realtime/FilteredListenersTest.java
@@ -8,7 +8,9 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class FilteredListenersTest {
 
