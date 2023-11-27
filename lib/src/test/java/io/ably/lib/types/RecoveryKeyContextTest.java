c767de20bf4378d06f82f4f62679906a2a82c4f9
sacOO7 <sachinshinde7676@gmail.com>
2023-11-27 17:32:35 +0530

    Added test for encoding and decoding recovery key

--- lib/src/test/java/io/ably/lib/types/RecoveryKeyContextTest.java
+++ lib/src/test/java/io/ably/lib/types/RecoveryKeyContextTest.java
@@ -6,9 +6,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class RecoveryKeyContextTest {
 
+    /**
+     * Spec: RTN16i, RTN16f, RTN16j
+     */
     @Test
     public void should_encode_recovery_key_context_object() {
         String expectedRecoveryKey =
@@ -20,8 +24,37 @@ public class RecoveryKeyContextTest {
         keys.put("channel3", "3");
         recoveryKey.setChannelSerials(keys);
         String encodedRecoveryKey = recoveryKey.encode();
-        assertEquals("should be equal", expectedRecoveryKey, encodedRecoveryKey);
+        assertEquals(expectedRecoveryKey, encodedRecoveryKey);
     }
 
+    /**
+     * Spec: RTN16i, RTN16f, RTN16j
+     */
+    @Test
+    public void should_decode_recoverykey_to_recoveryKeyContextObject() {
+         String recoveryKey =
+            "{\"connectionKey\":\"key2\",\"msgSerial\":5,\"channelSerials\":{\"channel1\":\"98\",\"channel2\":\"32\",\"channel3\":\"09\"}}";
+        RecoveryKeyContext recoveryKeyContext = RecoveryKeyContext.decode(recoveryKey);
+        assertEquals("key2", recoveryKeyContext.getConnectionKey());
+        assertEquals(5, recoveryKeyContext.getMsgSerial());
+        Map<String, String> expectedChannelSerials = new HashMap<String, String>()
+        {{
+            put("channel1", "98");
+            put("channel2", "32");
+            put("channel3", "09");
+        }};
+        assertEquals(expectedChannelSerials, recoveryKeyContext.getChannelSerials());
+    }
+
+    /**
+     * Spec: RTN16i, RTN16f, RTN16j
+     */
+    @Test
+    public void should_return_null_recovery_context_while_decoding_faulty_recovery_key() {
+        String recoveryKey =
+            "{\"connectionKey\":\"key2\",\"msgSerial\":\"incorrectStringSerial\",\"channelSerials\":{\"channel1\":\"98\",\"channel2\":\"32\",\"channel3\":\"09\"}}";
+        RecoveryKeyContext recoveryKeyContext = RecoveryKeyContext.decode(recoveryKey);
+        assertNull(recoveryKeyContext);
+    }
 
 }
