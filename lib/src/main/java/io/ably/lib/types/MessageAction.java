642f7ef62aff2918539fc311be0f9e44ea2e781f
evgeny <khokhlov.e.n@gmail.com>
2025-01-09 12:29:47 +0000

    [ECO-5188] fix: message actions based on specification update
    [Spec Update](https://github.com/ably/specification/pull/263/files)

--- lib/src/main/java/io/ably/lib/types/MessageAction.java
+++ lib/src/main/java/io/ably/lib/types/MessageAction.java
@@ -1,13 +1,11 @@
 package io.ably.lib.types;
 
 public enum MessageAction {
-    MESSAGE_UNSET, // 0
-    MESSAGE_CREATE, // 1
-    MESSAGE_UPDATE, // 2
-    MESSAGE_DELETE, // 3
-    ANNOTATION_CREATE, // 4
-    ANNOTATION_DELETE, // 5
-    META_OCCUPANCY; // 6
+    MESSAGE_CREATE, // 0
+    MESSAGE_UPDATE, // 1
+    MESSAGE_DELETE, // 2
+    META_OCCUPANCY, // 3
+    MESSAGE_SUMMARY; // 4
 
     static MessageAction tryFindByOrdinal(int ordinal) {
         return values().length <= ordinal ? null: values()[ordinal];
