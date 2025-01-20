172b5747b08cf6784f20ecb37c15d7e020e61e6c
sacOO7 <sachinshinde7676@gmail.com>
2025-01-20 19:33:03 +0530

    [ECO-5193][TM*] Updated ChaneMessagesTest.java 1. Added test to check for updated room message

--- lib/src/test/java/io/ably/lib/chat/ChatMessagesTest.java
+++ lib/src/test/java/io/ably/lib/chat/ChatMessagesTest.java
@@ -108,4 +108,118 @@ public class ChatMessagesTest extends ParameterizedTest {
                 ably.close();
         }
     }
+
+    /**
+     * Test that a message updated via rest API is sent to a messages channel.
+     * It should be received by another client that is subscribed to the same messages channel.
+     * Make sure to use two clientIds: clientId1 and clientId2
+     */
+    @Test
+    public void test_room_message_is_updated() {
+        String roomId = "1234";
+        String channelName = roomId + "::$chat::$chatMessages";
+        AblyRealtime ablyClient1 = null;
+        AblyRealtime ablyClient2 = null;
+        try {
+            ClientOptions opts1 = createOptions(testVars.keys[7].keyStr);
+            opts1.clientId = "clientId1";
+            ablyClient1 = new AblyRealtime(opts1);
+
+            ClientOptions opts2 = createOptions(testVars.keys[7].keyStr);
+            opts2.clientId = "clientId2";
+            ablyClient2 = new AblyRealtime(opts2);
+
+            ChatRoom room = new ChatRoom(roomId, ablyClient1);
+
+            // Create a channel and attach with client1
+            final Channel channel1 = ablyClient1.channels.get(channelName);
+            channel1.attach();
+            (new Helpers.ChannelWaiter(channel1)).waitFor(ChannelState.attached);
+
+            // Subscribe to messages with client2
+            final Channel channel2 = ablyClient2.channels.get(channelName);
+            channel2.attach();
+            (new Helpers.ChannelWaiter(channel2)).waitFor(ChannelState.attached);
+
+            List<Message> receivedMsg = new ArrayList<>();
+            channel2.subscribe(receivedMsg::add);
+
+            // Send message to room
+            ChatRoom.SendMessageParams params = new ChatRoom.SendMessageParams();
+            params.text = "hello there";
+            JsonObject sendMessageResult = (JsonObject) room.sendMessage(params);
+            String originalSerial = sendMessageResult.get("serial").getAsString();
+            String originalCreatedAt = sendMessageResult.get("createdAt").getAsString();
+
+            // Wait for the message to be received
+            Exception err = new Helpers.ConditionalWaiter().wait(() -> !receivedMsg.isEmpty(), 10_000);
+            Assert.assertNull(err);
+
+            // Update the message
+            ChatRoom.UpdateMessageParams updateParams = new ChatRoom.UpdateMessageParams();
+            // Update message context
+            updateParams.message = new ChatRoom.SendMessageParams();
+            updateParams.message.text = "updated text";
+            JsonObject metaData = new JsonObject();
+            JsonObject foo = new JsonObject();
+            foo.addProperty("bar", 1);
+            metaData.add("foo", foo);
+            updateParams.message.metadata = metaData;
+            // Update description
+            updateParams.description = "message updated by clientId1";
+
+            // TODO - Update external metadata, this will be populated in operation field
+            // updateParams.metadata = params.metadata;
+
+            JsonObject updateMessageResult = (JsonObject) room.updateMessage(originalSerial, updateParams);
+            String updateResultVersion = updateMessageResult.get("version").getAsString();
+            String updateResultTimestamp = updateMessageResult.get("timestamp").getAsString();
+
+            // Wait for the updated message to be received
+            err = new Helpers.ConditionalWaiter().wait(() -> receivedMsg.size() == 2, 10_000);
+            Assert.assertNull(err);
+
+            // Verify the updated message
+            Message updatedMessage = receivedMsg.get(1);
+
+            Assert.assertEquals(MessageAction.MESSAGE_UPDATE, updatedMessage.action);
+
+            Assert.assertFalse("Message ID should not be empty", updatedMessage.id.isEmpty());
+            Assert.assertEquals("chat.message", updatedMessage.name);
+            Assert.assertEquals("clientId1", updatedMessage.clientId);
+
+            JsonObject data = (JsonObject) updatedMessage.data;
+            Assert.assertEquals(2, data.entrySet().size());
+            Assert.assertEquals("updated text", data.get("text").getAsString());
+            JsonObject metadata = data.getAsJsonObject("metadata");
+            Assert.assertTrue(metadata.has("foo"));
+            Assert.assertTrue(metadata.get("foo").isJsonObject());
+            Assert.assertEquals(1, metadata.getAsJsonObject("foo").get("bar").getAsInt());
+
+            Assert.assertEquals(originalSerial, updatedMessage.serial);
+            Assert.assertEquals(updateResultVersion, updatedMessage.version);
+
+            Assert.assertEquals(originalCreatedAt, updatedMessage.createdAt.toString());
+            Assert.assertEquals(updateResultTimestamp, String.valueOf(updatedMessage.timestamp));
+
+            // TODO - Add assertion for operation field
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("Unexpected exception instantiating library");
+        } finally {
+            if (ablyClient1 != null) ablyClient1.close();
+            if (ablyClient2 != null) ablyClient2.close();
+        }
+    }
+
+    /**
+     * Test that a message deleted via rest API is sent to a messages channel.
+     * It should be received by another client that is subscribed to the same messages channel.
+     * Make sure to use two clientIds: clientId1 and clientId2
+     */
+    @Test
+    public void test_room_message_is_deleted() {
+
+    }
 }
