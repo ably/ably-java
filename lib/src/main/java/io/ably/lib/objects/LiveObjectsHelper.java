d388c15d3afdacea3649e7ea9d3b2cfcbdeac1ee
sacOO7 <sachinshinde7676@gmail.com>
2025-06-16 19:59:45 +0530

    [ECO-5386] Defined LiveObjectSerializer interface 1. Added LiveObjectsHelper for initializing LiveObjects plugin and LiveObjectsSerializer impl. 2. Updated ProtocolMessage to handle msgpack serialization/deserialization for state field 3. Implemented LiveObjectsJsonSerializer for json serialization/deserialization of state field

---
+++ lib/src/main/java/io/ably/lib/objects/LiveObjectsHelper.java
@@ -0,0 +1,40 @@
+package io.ably.lib.objects;
+
+import io.ably.lib.realtime.AblyRealtime;
+import io.ably.lib.util.Log;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class LiveObjectsHelper {
+
+    private static final String TAG = LiveObjectsHelper.class.getName();
+    private static LiveObjectSerializer liveObjectSerializer;
+
+    public static LiveObjectsPlugin tryInitializeLiveObjectsPlugin(AblyRealtime ablyRealtime) {
+        try {
+            Class<?> liveObjectsImplementation = Class.forName("io.ably.lib.objects.DefaultLiveObjectsPlugin");
+            LiveObjectsAdapter adapter = new Adapter(ablyRealtime);
+            return (LiveObjectsPlugin) liveObjectsImplementation
+                .getDeclaredConstructor(LiveObjectsAdapter.class)
+                .newInstance(adapter);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException |
+                 InvocationTargetException e) {
+            Log.i(TAG, "LiveObjects plugin not found in classpath. LiveObjects functionality will not be available.", e);
+            return null;
+        }
+    }
+
+    public static LiveObjectSerializer getLiveObjectSerializer() {
+        if (liveObjectSerializer == null) {
+            try {
+                Class<?> serializerClass = Class.forName("io.ably.lib.objects.DefaultLiveObjectSerializer");
+                liveObjectSerializer = (LiveObjectSerializer) serializerClass.getDeclaredConstructor().newInstance();
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException |
+                     InvocationTargetException e) {
+                Log.e(TAG, "Failed to init LiveObjectSerializer, LiveObjects plugin not included in the classpath", e);
+                return null;
+            }
+        }
+        return liveObjectSerializer;
+    }
+}
--- live-objects/src/main/kotlin/io/ably/lib/objects/Serialization.kt
+++ live-objects/src/main/kotlin/io/ably/lib/objects/Serialization.kt
@@ -2,9 +2,30 @@ package io.ably.lib.objects
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonArray
+import org.msgpack.core.MessagePacker
+import org.msgpack.core.MessageUnpacker
 
 internal val gson: Gson = createGsonSerializer()
 
 private fun createGsonSerializer(): Gson {
   return GsonBuilder().create() // Do not call serializeNulls() to omit null values
+}
+
+internal class DefaultLiveObjectSerializer : LiveObjectSerializer {
+  override fun readMsgpackArray(unpacker: MessageUnpacker): Array<Any> {
+    TODO("Not yet implemented")
+  }
+
+  override fun writeMsgpackArray(objects: Array<out Any>?, packer: MessagePacker) {
+    TODO("Not yet implemented")
+  }
+
+  override fun readFromJsonArray(json: JsonArray): Array<Any> {
+    TODO("Not yet implemented")
+  }
+
+  override fun asJsonArray(objects: Array<out Any>?): JsonArray {
+    TODO("Not yet implemented")
+  }
 }
