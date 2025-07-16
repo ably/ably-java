e12229432a9f4899149c24a97cfb79d80f3a213c
evgeny <khokhlov.e.n@gmail.com>
2025-07-16 18:13:00 +0100

    fix: move serializer initialization inside methods
    it prevents SDK from immediately posting an error log message about not having LiveObject installed

--- lib/src/main/java/io/ably/lib/objects/LiveObjectsJsonSerializer.java
+++ lib/src/main/java/io/ably/lib/objects/LiveObjectsJsonSerializer.java
@@ -13,10 +13,10 @@ import java.lang.reflect.Type;
 
 public class LiveObjectsJsonSerializer implements JsonSerializer<Object[]>, JsonDeserializer<Object[]> {
     private static final String TAG = LiveObjectsJsonSerializer.class.getName();
-    private final LiveObjectSerializer serializer = LiveObjectsHelper.getLiveObjectSerializer();
 
     @Override
     public Object[] deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        LiveObjectSerializer serializer = LiveObjectsHelper.getLiveObjectSerializer();
         if (serializer == null) {
             Log.w(TAG, "Skipping 'state' field json deserialization because LiveObjectsSerializer not found.");
             return null;
@@ -29,6 +29,7 @@ public class LiveObjectsJsonSerializer implements JsonSerializer<Object[]>, Json
 
     @Override
     public JsonElement serialize(Object[] src, Type typeOfSrc, JsonSerializationContext context) {
+        LiveObjectSerializer serializer = LiveObjectsHelper.getLiveObjectSerializer();
         if (serializer == null) {
             Log.w(TAG, "Skipping 'state' field json serialization because LiveObjectsSerializer not found.");
             return JsonNull.INSTANCE;
