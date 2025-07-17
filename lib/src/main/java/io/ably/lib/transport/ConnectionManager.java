1807693dcb2add3dc520ba29dd2dfef2bec966d7
evgeny <khokhlov.e.n@gmail.com>
2025-07-17 12:49:24 +0100

    fix: reset `msgSerial` on reconnect after suspension
    - Ensure `msgSerial` is reset to `0` when reconnecting from the suspended state.
    - Added a unit test to verify this behavior.

--- lib/src/main/java/io/ably/lib/transport/ConnectionManager.java
+++ lib/src/main/java/io/ably/lib/transport/ConnectionManager.java
@@ -1285,6 +1285,8 @@ public class ConnectionManager implements ConnectListener {
                 addPendingMessagesToQueuedMessages(true);
                 channels.transferToChannelQueue(extractConnectionQueuePresenceMessages());
             }
+        } else {
+            msgSerial = 0;
         }
 
         connection.id = message.connectionId;
